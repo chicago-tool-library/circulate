72cc9c048da44e4dcafd93c29cac7b2057ba71bf
Jim Benton <jim@autonomousmachine.com>
2019-05-07 22:33:53 -0500

    Add responsive tables to most indexes.

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,4 +1,8 @@
 module CategoriesHelper
+  def full_category_parent_name(category)
+    category.ancestors.map(&:name).join(" > ")
+  end
+
   def full_category_name(category)
     (category.ancestors.map(&:name) << category.name).join(" > ")
   end
