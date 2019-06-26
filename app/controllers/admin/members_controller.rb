948bf7f1fa07509e40281da30cd3d2b96c681ab1
Jim Benton <jim@autonomousmachine.com>
2019-06-25 22:14:24 -0500

    Allow postal code as a param in the admin/members controller.

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -75,7 +75,7 @@ module Admin
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def member_params
-      params.require(:member).permit(:full_name, :preferred_name, :email, :pronoun, :custom_pronoun, :phone_number, :notes, :id_kind, :other_id_kind, :id_number, :address_verified)
+      params.require(:member).permit(:full_name, :preferred_name, :email, :pronoun, :custom_pronoun, :phone_number, :notes, :id_kind, :other_id_kind, :postal_code, :address_verified)
     end
   end
 end
