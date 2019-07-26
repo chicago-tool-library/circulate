4d902c04a3084c024b90a3de18bcecbe89efa2c3
Jim Benton <jim@autonomousmachine.com>
2019-07-25 23:18:17 -0500

    Add default host to mailer url options.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  config.action_mailer.default_url_options = { :host => 'app.chicagotoollibrary.org' }
+  config.action_mailer.asset_host = 'https://app.chicagotoollibrary.org'
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
