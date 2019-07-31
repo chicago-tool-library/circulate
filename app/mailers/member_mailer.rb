cde9fb1b7817b4bde50991ed844544c7f22c9142
Jim Benton <jim@autonomousmachine.com>
2019-07-30 19:35:28 -0500

    Fix signup email on little screens.

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -2,7 +2,6 @@ class MemberMailer < ApplicationMailer
   def welcome_message
     @member = params[:member]
     @amount = Money.new(params[:amount]) if params.key?(:amount)
-    @intro = "Welcome\nto the\nlibrary"
     @title = "Welcome to The Chicago Tool Library"
     mail(to: @member.email, subject: @title)
   end
