f299547f52f9ad80256d54e425b013a37dfdfe90
Jim Benton <jim@autonomousmachine.com>
2019-08-18 10:56:36 -0500

    Turns out this was just in the wrong place.

---
+++ app/controllers/admin/renewals_controller.rb
@@ -0,0 +1,10 @@
+module Admin		
+  class RenewalsController < BaseController		
+    def create		
+      @loan = Loan.find(params[:loan_id])		
+      @loan.renew!		
+
+       redirect_to admin_member_url(@loan.member_id, anchor: "checkout")		
+    end		
+  end		
+end
