cd2f847a74de02078e42ef647ee2e8606afc2f5b
Jim Benton <jim@autonomousmachine.com>
2019-10-12 23:30:51 -0500

    Fix typo in date format.

--- config/initializers/date_format.rb
+++ config/initializers/date_format.rb
@@ -2,4 +2,4 @@ Date::DATE_FORMATS[:short_date] = "%-m/%-d/%Y"
 Time::DATE_FORMATS[:short_date] = "%-m/%-d/%Y"
 
 Date::DATE_FORMATS[:short_datetime] = "%A %-m/%-d/%Y %-l:%M %P"
-Time::DATE_FORMATS[:short_datetime] = "%A -m/%-d/%Y %-l:%M %P"
+Time::DATE_FORMATS[:short_datetime] = "%A %-m/%-d/%Y %-l:%M %P"
