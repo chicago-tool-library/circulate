fa82904db72cd2d55343337fa4ce96b8c2b70663
Jim Benton <jim@autonomousmachine.com>
2019-10-18 20:29:41 -0500

    Clean up old webpacker assets.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,6 @@
 const { environment } = require('@rails/webpacker')
 
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+environment.plugins.prepend("CleanWebpackPlugin", new CleanWebpackPlugin());
+
 module.exports = environment
---
+++ lib/tasks/assets.rake
@@ -0,0 +1,5 @@
+namespace :assets do
+  task :clean do
+    puts "noop to keep Heroku happy"
+  end
+end
