a94c2383152c8973e76e8d3d9774121cc34e393f
Jim Benton <jim@autonomousmachine.com>
2019-10-21 16:02:34 -0500

    Create data for the monthly adjustments test.

--- test/controllers/admin/monthly_adjustments_controller_test.rb
+++ test/controllers/admin/monthly_adjustments_controller_test.rb
@@ -9,6 +9,11 @@ class MonthlyAdjustmentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
+    create(:fine_adjustment)
+    create(:membership_adjustment)
+    create(:cash_payment_adjustment)
+    create(:square_payment_adjustment)
+    
     get admin_monthly_adjustments_url
     assert_response :success
   end
