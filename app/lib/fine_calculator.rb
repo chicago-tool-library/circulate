46bb4c04388dc8c8de63134088fc5ab6ad42b9ac
Jim Benton <jim@autonomousmachine.com>
2019-10-26 19:39:35 -0500

    Show current fine for overdue items on member page.

--- app/lib/fine_calculator.rb
+++ app/lib/fine_calculator.rb
@@ -1,4 +1,9 @@
 class FineCalculator
+  def self.for_overdue_loan(loan, now)
+    borrow_policy = loan.item.borrow_policy
+    new.amount(fine: borrow_policy.fine, period: borrow_policy.fine_period, due_at: loan.due_at, returned_at: now)
+  end
+
   def amount(fine:, period:, due_at:, returned_at:)
     if returned_at <= due_at
       Money.new(0)
