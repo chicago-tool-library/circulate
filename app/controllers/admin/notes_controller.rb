d9392419ad487844059e14d240bb9004d6af1003
Jim Benton <jim@autonomousmachine.com>
2019-11-29 14:52:34 -0600

    Add and edit notes on items.

--- app/controllers/admin/notes_controller.rb
+++ app/controllers/admin/notes_controller.rb
@@ -1,5 +1,6 @@
 class Admin::NotesController < ApplicationController
   include ActionView::RecordIdentifier
+  include PortalRendering
 
   before_action :load_parent
 
@@ -7,10 +8,30 @@ class Admin::NotesController < ApplicationController
     @note = @parent.notes.create(note_params.merge(creator: current_user))
 
     if @note.save
-      redirect_to [:admin, @parent], anchor: dom_id(@note)
+      redirect_to [:admin, @parent, anchor: dom_id(@note)]
     else
-      render "new"
+      render_to_portal "form", locals: {parent: @parent, note: @note}, status: 422
     end
+  end
+
+  def update
+    @note = @parent.notes.find(params[:id])
+
+    if @note.update(note_params)
+      redirect_to [:admin, @parent, anchor: dom_id(@note)]
+    else
+      render_to_portal "form", locals: {parent: @parent, note: @note}, status: 422
+    end
+  end
+
+  def edit
+    @note = @parent.notes.find(params[:id])
+    render_to_portal "form", locals: {parent: @parent, note: @note}
+  end
+
+  def show
+    @note = @parent.notes.find(params[:id])
+    render_to_portal "show", locals: {parent: @parent, note: @note}
   end
 
   private
--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,6 @@ class Note < ApplicationRecord
   belongs_to :creator, class_name: "User"
 
   has_rich_text :body
+
+  validates :body, presence: true
 end
