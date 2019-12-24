4ee3c2c2c002015e8efb68e5bf777216a13eaba3
Jim Benton <jim@autonomousmachine.com>
2019-12-24 10:11:02 -0600

    Headless Chrome should write to /tmp instead.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -20,7 +20,7 @@ end
 
 Capybara.register_driver(:headless_chrome) do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {args: %w[headless disable-gpu no-sandbox]}
+    chromeOptions: {args: %w[headless disable-gpu no-sandbox disable-dev-shm-usage]}
   )
 
   Capybara::Selenium::Driver.new(
