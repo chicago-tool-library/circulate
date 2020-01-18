dc3b0d32c4d616a1e0531137a3f6f16876c628d1
Jim Benton <jim@autonomousmachine.com>
2020-01-18 15:59:30 -0600

    Remove controller concern that was used in a single place.

--- app/controllers/concerns/member_page.rb
+++
@@ -1,6 +0,0 @@
-module MemberPage
-  def load_member_page_data
-    @active_loan_summaries = @member.loan_summaries.checked_out.includes(:latest_loan, item: :borrow_policy).by_due_date
-    @recent_loan_summaries = @member.loan_summaries.recently_returned.includes(:adjustment, item: :borrow_policy).by_end_date.limit(10)
-  end
-end
