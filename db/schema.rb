aec547e63cd7d69fdd28016c370852f6cefd3e09
Jim Benton <jim@autonomousmachine.com>
2020-01-26 22:50:46 -0600

    Add renewal_limit column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_020026) do
+ActiveRecord::Schema.define(version: 2020_01_26_220929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_020026) do
     t.string "code", null: false
     t.string "description"
     t.boolean "default", default: false, null: false
+    t.integer "renewal_limit", default: 0, null: false
   end
 
   create_table "documents", force: :cascade do |t|
