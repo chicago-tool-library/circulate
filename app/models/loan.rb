5142773a9d3a24201584208a0374780987a1f0d8
Jim Benton <jim@autonomousmachine.com>
2020-06-05 19:34:31 -0500

    Prevent duplicate renewals.

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -7,6 +7,7 @@ class Loan < ApplicationRecord
 
   validates :due_at, presence: true
   validates_numericality_of :ended_at, allow_nil: true, greater_than_or_equal_to: ->(loan) { loan.created_at }
+  validates :initial_loan_id, uniqueness: {scope: :renewal_count}, if: ->(l) { l.initial_loan_id.present? }
 
   validates_each :item_id do |record, attr, value|
     if value
