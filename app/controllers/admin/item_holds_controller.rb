89944a7e1d2d7a053382507f06e99697a6de6b6e
Jim Benton <jim@autonomousmachine.com>
2020-06-20 00:23:42 -0500

    Hide inactive holds.

--- app/controllers/admin/item_holds_controller.rb
+++ app/controllers/admin/item_holds_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class ItemHoldsController < BaseController
     def index
       @item = Item.find(params[:item_id])
-      @holds = @item.holds.order(created_at: :asc)
+      @holds = @item.holds.active.order(created_at: :asc)
     end
   end
 end
