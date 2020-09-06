73244586d97ffe189a78c61ef5a5319fca4aacef
Amy Gurski <amygurski@gmail.com>
2020-09-06 17:14:09 -0400

    adjusts history view to match mockup

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,6 +1,9 @@
 class MembersController < ApplicationController
   def history
-    @loans = current_user.loans
+    @loans = current_user.loans.order(ended_at: :desc)
+    # This could be appended to only show items that have been checked in
+    # if it's decided this page shouldn't show currently checked-out items:
+    # .where.not(loans: { ended_at: nil })
   end
 
   private
