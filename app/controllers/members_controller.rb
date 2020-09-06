7c6cd81e58ee5f6661a4b2dfcefbd3edb30f03c4
David Bertoglio <dbertoglio@taphere.com>
2020-09-06 16:16:17 -0400

    Use current_member instead of temporary helper
    Switched to using the current_member helper over the temporarily
    hardcoded current_user value to get the currently logging in member.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,5 @@
 class MembersController < ApplicationController
   def loans
-    @loans = current_user.loans.includes(:item).order(:due_at)
-  end
-
-  # Temporarily overriding devise method for testing purposes
-  def current_user
-    Member.first
+    @loans = current_member.loans.includes(:item).order(:due_at)
   end
 end
