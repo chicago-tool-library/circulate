5e72b8029f8aa8e93bd8095047e0e7b3139bff5f
britth <brittany@spreedly.com>
2020-09-12 10:09:47 -0400

    edit action updates

--- app/controllers/member_profiles_controller.rb
+++ app/controllers/member_profiles_controller.rb
@@ -9,11 +9,11 @@ class MemberProfilesController < ApplicationController
     def update
       respond_to do |format|
         if current_member.update(member_params)
-          format.html { redirect_to :member_profile, success: "Item was successfully updated." }
+          format.html { redirect_to :member_profile, success: "Profile was successfully updated." }
           format.json { render :show, status: :ok, location: current_member }
         else
           format.html { render :edit }
-          format.json { render json: @item.errors, status: :unprocessable_entity }
+          format.json { render json: @member.errors, status: :unprocessable_entity }
         end
       end
     end
