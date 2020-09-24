4892946f609f4d1bd5e0a02bbd8f860ea1110ace
Neil L. Decapia <neildecapia@gmail.com>
2020-09-24 22:47:07 +0800

    Validate member postal code against library-specified pattern
    [#114]

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,2 +1,7 @@
 class Library < ApplicationRecord
+  def allows_postal_code?(postal_code)
+    return true if postal_code.blank?
+
+    /#{member_postal_code_pattern}/ =~ postal_code.to_s
+  end
 end
