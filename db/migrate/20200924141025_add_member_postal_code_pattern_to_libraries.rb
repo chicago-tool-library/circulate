967092d00fed628dd4021cf8631a97672520f904
Neil L. Decapia <neildecapia@gmail.com>
2020-09-24 23:12:02 +0800

    Set postal code pattern for Library Zero
    [#114]

--- db/migrate/20200924141025_add_member_postal_code_pattern_to_libraries.rb
+++ db/migrate/20200924141025_add_member_postal_code_pattern_to_libraries.rb
@@ -1,5 +1,14 @@
 class AddMemberPostalCodePatternToLibraries < ActiveRecord::Migration[6.0]
   def change
     add_column :libraries, :member_postal_code_pattern, :string, limit: 100, null: true, default: nil
+
+    library_zero_id = select_value("SELECT id FROM libraries WHERE hostname = 'chicagotoollibrary.herokuapp.com'").presence
+    return unless library_zero_id
+
+    reversible do |dir|
+      dir.up do
+        execute "UPDATE libraries SET member_postal_code_pattern = '60707|60827|^606' WHERE id = #{library_zero_id}"
+      end
+    end
   end
 end
