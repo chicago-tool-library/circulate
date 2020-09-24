368a9a30f6a701b388f174465fc4ba10d81ee9fb
Rafael Garcia <rafbgarcia@gmail.com>
2020-09-24 15:21:26 -0400

    Test edge case and finish the other method's test

--- app/lib/open_days.rb
+++ app/lib/open_days.rb
@@ -5,7 +5,7 @@ class OpenDays
   ].freeze
 
   def self.next_slots(weeks: 2, time_slots: TIME_SLOTS)
-    weeks.times.flat_map do |weeks_from_now|
+    weeks.times.flat_map { |weeks_from_now|
       time_slots.flat_map do |slot|
         (slot.from...slot.to).map do |time|
           if time == 12
@@ -17,14 +17,14 @@ class OpenDays
           end
         end
       end
-    end
+    }.sort
   end
 
-  def self.next_slots_for_select(weeks: 2)
-    next_slots(weeks: weeks).each_with_object({}) do |date, memo|
-      memo[date.strftime("%a %b %e")] ||= []
-      memo[date.strftime("%a %b %e")] << [
-        "#{date.strftime("%l %P")} to #{(date + 1.hour).strftime("%l %P")}",
+  def self.next_slots_for_select(weeks: 2, time_slots: TIME_SLOTS)
+    next_slots(weeks: weeks, time_slots: time_slots).each_with_object({}) do |date, memo|
+      memo[date.strftime("%a %b %-e")] ||= []
+      memo[date.strftime("%a %b %-e")] << [
+        "#{date.strftime("%-l %P")} to #{(date + 1.hour).strftime("%-l %P")}",
         date..date + 1.hour
       ]
     end
--- test/lib/open_days_test.rb
+++ test/lib/open_days_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class ActivityNotifierTest < ActiveSupport::TestCase
   TIME_SLOTS = [
     OpenStruct.new(day: "Thursday", from: 18, to: 20),
-    OpenStruct.new(day: "Saturday", from: 11, to: 13),
+    OpenStruct.new(day: "Saturday", from: 11, to: 13)
   ].freeze
 
   test "returns possible time slots" do
@@ -11,19 +11,62 @@ class ActivityNotifierTest < ActiveSupport::TestCase
       slots = OpenDays.next_slots(weeks: 2, time_slots: TIME_SLOTS)
 
       assert_equal slots.count, 8
-      assert_equal slots[0].to_s, Time.local(2020, 9, 24, 18, 0, 0).to_s
-      assert_equal slots[1].to_s, Time.local(2020, 9, 24, 19, 0, 0).to_s
-      assert_equal slots[2].to_s, Time.local(2020, 9, 26, 11, 0, 0).to_s
-      assert_equal slots[3].to_s, Time.local(2020, 9, 26, 12, 0, 0).to_s
+      assert_equal slots[0].to_s, Time.zone.local(2020, 9, 24, 18, 0, 0).to_s
+      assert_equal slots[1].to_s, Time.zone.local(2020, 9, 24, 19, 0, 0).to_s
+      assert_equal slots[2].to_s, Time.zone.local(2020, 9, 26, 11, 0, 0).to_s
+      assert_equal slots[3].to_s, Time.zone.local(2020, 9, 26, 12, 0, 0).to_s
 
-      assert_equal slots[4].to_s, Time.local(2020, 10, 1, 18, 0, 0).to_s
-      assert_equal slots[5].to_s, Time.local(2020, 10, 1, 19, 0, 0).to_s
-      assert_equal slots[6].to_s, Time.local(2020, 10, 3, 11, 0, 0).to_s
-      assert_equal slots[7].to_s, Time.local(2020, 10, 3, 12, 0, 0).to_s
+      assert_equal slots[4].to_s, Time.zone.local(2020, 10, 1, 18, 0, 0).to_s
+      assert_equal slots[5].to_s, Time.zone.local(2020, 10, 1, 19, 0, 0).to_s
+      assert_equal slots[6].to_s, Time.zone.local(2020, 10, 3, 11, 0, 0).to_s
+      assert_equal slots[7].to_s, Time.zone.local(2020, 10, 3, 12, 0, 0).to_s
+    end
+  end
+
+  # This handles an edge case:
+  # We're asking for time slots for Thursday and Saturday,
+  # if today is Friday, it's going to parse first the date for the
+  # next Wednesday (which is only next week) and then the date for
+  # the next Saturday which is earlier than the next Wednesday.
+  #
+  test "returns dates in ascending order" do
+    Timecop.freeze(DateTime.new(2020, 9, 25, 14, 0, 0)) do
+      slots = OpenDays.next_slots(weeks: 2, time_slots: TIME_SLOTS)
+
+      assert_equal slots.count, 8
+      assert_equal slots[0].to_s, Time.zone.local(2020, 9, 26, 11, 0, 0).to_s
+      assert_equal slots[1].to_s, Time.zone.local(2020, 9, 26, 12, 0, 0).to_s
+
+      assert_equal slots[2].to_s, Time.zone.local(2020, 10, 1, 18, 0, 0).to_s
+      assert_equal slots[3].to_s, Time.zone.local(2020, 10, 1, 19, 0, 0).to_s
+      assert_equal slots[4].to_s, Time.zone.local(2020, 10, 3, 11, 0, 0).to_s
+      assert_equal slots[5].to_s, Time.zone.local(2020, 10, 3, 12, 0, 0).to_s
+
+      assert_equal slots[6].to_s, Time.zone.local(2020, 10, 8, 18, 0, 0).to_s
+      assert_equal slots[7].to_s, Time.zone.local(2020, 10, 8, 19, 0, 0).to_s
     end
   end
 
   test "next_slots_for_select: returns formatted time slots" do
-    assert_equal OpenDays.next_slots_for_select(weeks: 2), {}
+    Timecop.freeze(DateTime.new(2020, 9, 24, 14, 0, 0)) do
+      assert_equal OpenDays.next_slots_for_select(weeks: 2, time_slots: TIME_SLOTS), {
+        "Sat Sep 26" => [
+          ["11 am to 12 pm", Time.parse("Sat, 26 Sep 2020 11:00:00 UTC +00:00")..Time.parse("Sat, 26 Sep 2020 12:00:00 UTC +00:00")],
+          ["12 pm to 1 pm", Time.parse("Sat, 26 Sep 2020 12:00:00 UTC +00:00")..Time.parse("Sat, 26 Sep 2020 13:00:00 UTC +00:00")]
+        ],
+        "Thu Oct 1" => [
+          ["6 pm to 7 pm", Time.parse("Thu, 01 Oct 2020 18:00:00 UTC +00:00")..Time.parse("Thu, 01 Oct 2020 19:00:00 UTC +00:00")],
+          ["7 pm to 8 pm", Time.parse("Thu, 01 Oct 2020 19:00:00 UTC +00:00")..Time.parse("Thu, 01 Oct 2020 20:00:00 UTC +00:00")]
+        ],
+        "Sat Oct 3" => [
+          ["11 am to 12 pm", Time.parse("Sat, 03 Oct 2020 11:00:00 UTC +00:00")..Time.parse("Sat, 03 Oct 2020 12:00:00 UTC +00:00")],
+          ["12 pm to 1 pm", Time.parse("Sat, 03 Oct 2020 12:00:00 UTC +00:00")..Time.parse("Sat, 03 Oct 2020 13:00:00 UTC +00:00")]
+        ],
+        "Thu Oct 8" => [
+          ["6 pm to 7 pm", Time.parse("Thu, 08 Oct 2020 18:00:00 UTC +00:00")..Time.parse("Thu, 08 Oct 2020 19:00:00 UTC +00:00")],
+          ["7 pm to 8 pm", Time.parse("Thu, 08 Oct 2020 19:00:00 UTC +00:00")..Time.parse("Thu, 08 Oct 2020 20:00:00 UTC +00:00")]
+        ]
+      }
+    end
   end
 end
