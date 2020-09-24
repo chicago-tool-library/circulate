bbea91ae9e10485b3ec32ad610a7ee3e1a6da3e0
Greg Blake <greg.blake@powerhrg.com>
2020-09-24 14:04:09 -0400

    Validates that Appointment#ends_at is *after* Appointment#starts_at

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,4 +6,16 @@ class Appointment < ApplicationRecord
   belongs_to :member
 
   validates :starts_at, :ends_at, presence: true
+
+  validate :ends_at_later_than_starts_at
+
+  private
+
+  def ends_at_later_than_starts_at
+    return if ends_at.blank? || starts_at.blank?
+
+    if ends_at < starts_at
+      errors.add(:ends_at, "must be after the starts_at date")
+    end
+  end
 end
