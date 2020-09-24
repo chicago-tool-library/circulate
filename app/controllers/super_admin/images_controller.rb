909d6c78f1cbd671117ec6fb5e7b00ae72586ded
Kyle Fadigan <kyle.fadigan@powerhrg.com>
2020-09-24 16:51:56 -0400

    Add images controller to super_admin

---
+++ app/controllers/super_admin/images_controller.rb
@@ -0,0 +1,29 @@
+module Admin
+  class ImagesController < BaseController
+    before_action :load_image
+
+    def edit
+    end
+
+    def update
+      @image.metadata["rotation"] = image_params[:rotation]
+      @image.attachment.blob.save!
+
+      redirect_to admin_item_url(@item)
+    end
+
+    private
+
+    def image_params
+      params.require(:image).permit(:rotation)
+    end
+
+    def load_image
+      @item = Item.find(params[:item_id])
+      unless @item.image.attached?
+        redirect_to admin_item_path(@item), error: "Image not found"
+      end
+      @image = @item.image
+    end
+  end
+end
