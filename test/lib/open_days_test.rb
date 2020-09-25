b43fee404c4f7d60278e93737353a63354e95e91
Greg Blake <greg.blake@powerhrg.com>
2020-09-25 07:55:32 -0400

    Uses ActiveSupport::Testing::TimeHelpers instead of TimeCop

--- test/lib/open_days_test.rb
+++ test/lib/open_days_test.rb
@@ -7,20 +7,20 @@ class OpenDaysTest < ActiveSupport::TestCase
   ].freeze
 
   test "returns possible time slots" do
-    Timecop.freeze(DateTime.new(2020, 9, 21)) do
-      slots = OpenDays.next_slots(weeks: 2, time_slots: TIME_SLOTS)
+    travel_to DateTime.new(2020, 9, 21)
+    slots = OpenDays.next_slots(weeks: 2, time_slots: TIME_SLOTS)
 
-      assert_equal slots.count, 8
-      assert_equal slots[0].to_s, Time.zone.local(2020, 9, 24, 18, 0, 0).to_s
-      assert_equal slots[1].to_s, Time.zone.local(2020, 9, 24, 19, 0, 0).to_s
-      assert_equal slots[2].to_s, Time.zone.local(2020, 9, 26, 11, 0, 0).to_s
-      assert_equal slots[3].to_s, Time.zone.local(2020, 9, 26, 12, 0, 0).to_s
+    assert_equal slots.count, 8
+    assert_equal slots[0].to_s, Time.zone.local(2020, 9, 24, 18, 0, 0).to_s
+    assert_equal slots[1].to_s, Time.zone.local(2020, 9, 24, 19, 0, 0).to_s
+    assert_equal slots[2].to_s, Time.zone.local(2020, 9, 26, 11, 0, 0).to_s
+    assert_equal slots[3].to_s, Time.zone.local(2020, 9, 26, 12, 0, 0).to_s
 
-      assert_equal slots[4].to_s, Time.zone.local(2020, 10, 1, 18, 0, 0).to_s
-      assert_equal slots[5].to_s, Time.zone.local(2020, 10, 1, 19, 0, 0).to_s
-      assert_equal slots[6].to_s, Time.zone.local(2020, 10, 3, 11, 0, 0).to_s
-      assert_equal slots[7].to_s, Time.zone.local(2020, 10, 3, 12, 0, 0).to_s
-    end
+    assert_equal slots[4].to_s, Time.zone.local(2020, 10, 1, 18, 0, 0).to_s
+    assert_equal slots[5].to_s, Time.zone.local(2020, 10, 1, 19, 0, 0).to_s
+    assert_equal slots[6].to_s, Time.zone.local(2020, 10, 3, 11, 0, 0).to_s
+    assert_equal slots[7].to_s, Time.zone.local(2020, 10, 3, 12, 0, 0).to_s
+    travel_back
   end
 
   # This handles an edge case:
@@ -30,43 +30,43 @@ class OpenDaysTest < ActiveSupport::TestCase
   # the next Saturday which is earlier than the next Wednesday.
   #
   test "returns dates in ascending order" do
-    Timecop.freeze(DateTime.new(2020, 9, 25, 14, 0, 0)) do
-      slots = OpenDays.next_slots(weeks: 2, time_slots: TIME_SLOTS)
+    travel_to DateTime.new(2020, 9, 25, 14, 0, 0)
+    slots = OpenDays.next_slots(weeks: 2, time_slots: TIME_SLOTS)
 
-      assert_equal slots.count, 8
-      assert_equal slots[0].to_s, Time.zone.local(2020, 9, 26, 11, 0, 0).to_s
-      assert_equal slots[1].to_s, Time.zone.local(2020, 9, 26, 12, 0, 0).to_s
+    assert_equal slots.count, 8
+    assert_equal slots[0].to_s, Time.zone.local(2020, 9, 26, 11, 0, 0).to_s
+    assert_equal slots[1].to_s, Time.zone.local(2020, 9, 26, 12, 0, 0).to_s
 
-      assert_equal slots[2].to_s, Time.zone.local(2020, 10, 1, 18, 0, 0).to_s
-      assert_equal slots[3].to_s, Time.zone.local(2020, 10, 1, 19, 0, 0).to_s
-      assert_equal slots[4].to_s, Time.zone.local(2020, 10, 3, 11, 0, 0).to_s
-      assert_equal slots[5].to_s, Time.zone.local(2020, 10, 3, 12, 0, 0).to_s
+    assert_equal slots[2].to_s, Time.zone.local(2020, 10, 1, 18, 0, 0).to_s
+    assert_equal slots[3].to_s, Time.zone.local(2020, 10, 1, 19, 0, 0).to_s
+    assert_equal slots[4].to_s, Time.zone.local(2020, 10, 3, 11, 0, 0).to_s
+    assert_equal slots[5].to_s, Time.zone.local(2020, 10, 3, 12, 0, 0).to_s
 
-      assert_equal slots[6].to_s, Time.zone.local(2020, 10, 8, 18, 0, 0).to_s
-      assert_equal slots[7].to_s, Time.zone.local(2020, 10, 8, 19, 0, 0).to_s
-    end
+    assert_equal slots[6].to_s, Time.zone.local(2020, 10, 8, 18, 0, 0).to_s
+    assert_equal slots[7].to_s, Time.zone.local(2020, 10, 8, 19, 0, 0).to_s
+    travel_back
   end
 
   test "next_slots_for_select: returns formatted time slots" do
-    Timecop.freeze(DateTime.new(2020, 9, 24, 14, 0, 0)) do
-      assert_equal OpenDays.next_slots_for_select(weeks: 2, time_slots: TIME_SLOTS), {
-        "Sat Sep 26" => [
-          ["11 am to 12 pm", Time.parse("Sat, 26 Sep 2020 11:00:00 UTC +00:00")..Time.parse("Sat, 26 Sep 2020 12:00:00 UTC +00:00")],
-          ["12 pm to 1 pm", Time.parse("Sat, 26 Sep 2020 12:00:00 UTC +00:00")..Time.parse("Sat, 26 Sep 2020 13:00:00 UTC +00:00")]
-        ],
-        "Thu Oct 1" => [
-          ["6 pm to 7 pm", Time.parse("Thu, 01 Oct 2020 18:00:00 UTC +00:00")..Time.parse("Thu, 01 Oct 2020 19:00:00 UTC +00:00")],
-          ["7 pm to 8 pm", Time.parse("Thu, 01 Oct 2020 19:00:00 UTC +00:00")..Time.parse("Thu, 01 Oct 2020 20:00:00 UTC +00:00")]
-        ],
-        "Sat Oct 3" => [
-          ["11 am to 12 pm", Time.parse("Sat, 03 Oct 2020 11:00:00 UTC +00:00")..Time.parse("Sat, 03 Oct 2020 12:00:00 UTC +00:00")],
-          ["12 pm to 1 pm", Time.parse("Sat, 03 Oct 2020 12:00:00 UTC +00:00")..Time.parse("Sat, 03 Oct 2020 13:00:00 UTC +00:00")]
-        ],
-        "Thu Oct 8" => [
-          ["6 pm to 7 pm", Time.parse("Thu, 08 Oct 2020 18:00:00 UTC +00:00")..Time.parse("Thu, 08 Oct 2020 19:00:00 UTC +00:00")],
-          ["7 pm to 8 pm", Time.parse("Thu, 08 Oct 2020 19:00:00 UTC +00:00")..Time.parse("Thu, 08 Oct 2020 20:00:00 UTC +00:00")]
-        ]
-      }
-    end
+    travel_to DateTime.new(2020, 9, 24, 14, 0, 0)
+    assert_equal OpenDays.next_slots_for_select(weeks: 2, time_slots: TIME_SLOTS), {
+      "Sat Sep 26" => [
+        ["11 am to 12 pm", Time.parse("Sat, 26 Sep 2020 11:00:00 UTC +00:00")..Time.parse("Sat, 26 Sep 2020 12:00:00 UTC +00:00")],
+        ["12 pm to 1 pm", Time.parse("Sat, 26 Sep 2020 12:00:00 UTC +00:00")..Time.parse("Sat, 26 Sep 2020 13:00:00 UTC +00:00")]
+      ],
+      "Thu Oct 1" => [
+        ["6 pm to 7 pm", Time.parse("Thu, 01 Oct 2020 18:00:00 UTC +00:00")..Time.parse("Thu, 01 Oct 2020 19:00:00 UTC +00:00")],
+        ["7 pm to 8 pm", Time.parse("Thu, 01 Oct 2020 19:00:00 UTC +00:00")..Time.parse("Thu, 01 Oct 2020 20:00:00 UTC +00:00")]
+      ],
+      "Sat Oct 3" => [
+        ["11 am to 12 pm", Time.parse("Sat, 03 Oct 2020 11:00:00 UTC +00:00")..Time.parse("Sat, 03 Oct 2020 12:00:00 UTC +00:00")],
+        ["12 pm to 1 pm", Time.parse("Sat, 03 Oct 2020 12:00:00 UTC +00:00")..Time.parse("Sat, 03 Oct 2020 13:00:00 UTC +00:00")]
+      ],
+      "Thu Oct 8" => [
+        ["6 pm to 7 pm", Time.parse("Thu, 08 Oct 2020 18:00:00 UTC +00:00")..Time.parse("Thu, 08 Oct 2020 19:00:00 UTC +00:00")],
+        ["7 pm to 8 pm", Time.parse("Thu, 08 Oct 2020 19:00:00 UTC +00:00")..Time.parse("Thu, 08 Oct 2020 20:00:00 UTC +00:00")]
+      ]
+    }
+    travel_back
   end
 end
