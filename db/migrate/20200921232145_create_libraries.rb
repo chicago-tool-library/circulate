4fcd5caeb41769531af8eae8131adb1023ab99ee
Ben Langfeld <blangfeld@powerhrg.com>
2020-09-25 11:08:33 -0300

    Squash another migration

--- db/migrate/20200921232145_create_libraries.rb
+++ db/migrate/20200921232145_create_libraries.rb
@@ -2,7 +2,7 @@ class CreateLibraries < ActiveRecord::Migration[6.0]
   def change
     create_table :libraries do |t|
       t.string :name, null: false
-      t.string :hostname, index: {unique: true}
+      t.string :hostname, null: false, index: {unique: true}
       t.string :member_postal_code_pattern, limit: 100, null: true, default: nil
 
       t.timestamps
--- db/migrate/20200925135324_change_libraries_hostname_null.rb
+++
@@ -1,5 +0,0 @@
-class ChangeLibrariesHostnameNull < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :libraries, :hostname, false
-  end
-end
