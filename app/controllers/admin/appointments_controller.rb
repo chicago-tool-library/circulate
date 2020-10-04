b5b07fe864f8f5a64a5f04a557171b0c369ea4fe
mtayllan <messiastallan@gmail.com>
2020-10-04 19:10:20 -0300

    change the current_day method to an instance variable to avoid multiple method calls

--- app/controllers/admin/appointments_controller.rb
+++ app/controllers/admin/appointments_controller.rb
@@ -1,7 +1,8 @@
 module Admin
   class AppointmentsController < BaseController
     def index
-      @appointments = Appointment.where(starts_at: current_day.beginning_of_day..current_day.end_of_day)
+      @current_day = Date.parse(params[:day] ||= Date.today.to_s)
+      @appointments = Appointment.where(starts_at: @current_day.beginning_of_day..@current_day.end_of_day)
     end
 
     def show
@@ -14,16 +15,12 @@ module Admin
 
     private
 
-    helper_method def current_day
-      @current_day = Date.parse(params[:day] ||= Date.today.to_s)
-    end
-
     helper_method def previous_day
-      current_day - 1.day
+      @current_day - 1.day
     end
 
     helper_method def next_day
-      current_day + 1.day
+      @current_day + 1.day
     end
 
     helper_method def current_appointment
