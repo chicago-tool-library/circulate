6645bc0d119cb219e5aa8857d2c9ca3facbfa64e
Renuga Saraswathy <renugasaraswathy@gmail.com>
2020-10-06 07:59:49 +0530

    added form validation for appointment creation

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -2,8 +2,21 @@ require "test_helper"
 
 class AppointmentTest < ActiveSupport::TestCase
   test "creates an appointment" do
-    appointment = FactoryBot.create(:appointment)
-
+    appointment = FactoryBot.build(:appointment)
+    user = FactoryBot.create(:user)
+    member = FactoryBot.create(:member, user: user)
+    hold = FactoryBot.create(:hold, creator: member.user)
+    appointment.holds << hold
+    appointment.starts_at = "2020-10-05 7:00AM"
+    appointment.ends_at = "2020-10-05 8:00AM"
+    appointment.save
+    assert_equal "Ida B. Wells", appointment.holds.first.member.full_name
     assert_equal "My Comment", appointment.comment
+  end
+
+  test "invalid without hold or loan selected and date not given" do
+    appointment = Appointment.new
+    appointment.save
+    assert_equal appointment.errors[:base], ["Please select an item to pick-up or return for your appointment", "Please select a date and time for this appointment."]
   end
 end
