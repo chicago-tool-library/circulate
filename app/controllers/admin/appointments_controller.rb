f0b382a4d3b1384c3a1599b86b104235981ec3b5
fosterv2 <fosterv2@outlook.com>
2020-10-07 15:11:17 -0700

    Change admin/appointments show view to include a form for selecting items currently loaned to the member to be added to the check-in for the appointment

--- app/controllers/admin/appointments_controller.rb
+++ app/controllers/admin/appointments_controller.rb
@@ -34,6 +34,11 @@ module Admin
       Item.available.eager_load(:borrow_policy)
     end
 
+    helper_method def items_avalable_to_add_to_dropoff
+      # current_appointment.member.loans.checked_out.select {|loan| !appointment_return_items.include?(loan)}.map(&:item)
+      current_appointment.member.loans.checked_out
+    end
+
     helper_method def appointment_pickup_items
       current_appointment.holds
     end
