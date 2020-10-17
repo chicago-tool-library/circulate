6027f60be4634bbf013fd3ce46a74c0867de9024
Jim Benton <jim@autonomousmachine.com>
2020-10-17 14:56:55 -0500

    Simplify Account::HoldsController

--- app/controllers/account/holds_controller.rb
+++ app/controllers/account/holds_controller.rb
@@ -1,30 +1,16 @@
 module Account
   class HoldsController < BaseController
-    
     def create
-      new_hold_save
-      redirect_to item_path(item), flash: create_redirect_flash
-    end
+      @item = Item.find(params[:item_id])
+      @new_hold = Hold.new(item: item, member: current_member, creator: current_user)
 
-    private
-
-    delegate :save, to: :new_hold, prefix: true, private: true
-    delegate :persisted?, to: :new_hold, prefix: true, private: true
-
-    def item
-      @item ||= Item.find(params[:item_id])
-    end
-
-    def new_hold
-      @new_hold ||= Hold.new(item: item, member: current_member, creator: current_user)
-    end
-
-    def create_redirect_flash
-      if new_hold_persisted?
+      flash_message = if @new_hold.save
         {success: "Hold placed."}
       else
         {error: "Something went wrong!"}
       end
+
+      redirect_to item_path(item), flash: flash_message
     end
   end
 end
