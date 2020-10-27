4b9b9547bb13c48844b0e716751de5d81b54ce0a
Richard Kim <rkim00@users.noreply.github.com>
2020-10-27 11:14:00 -0400

    formatted date for pick-up
    added a comma after date, hyphen to en-dash, tightened spaces

--- app/helpers/holds_helper.rb
+++ app/helpers/holds_helper.rb
@@ -20,7 +20,7 @@ module HoldsHelper
     appointment = hold.upcoming_appointment
 
     if appointment
-      "Scheduled for pick-up at #{format_date(appointment.starts_at)} " +
+      "Scheduled for pick-up at #{format_date(appointment.starts_at)}, " +
       format_time_range(appointment.starts_at, appointment.ends_at)
     elsif previous_holds_count == 0
       "Ready for pickup. Schedule by #{format_date(hold.created_at + 7.days)}"
@@ -46,6 +46,6 @@ module HoldsHelper
   end
 
   def format_time_range(starts_at, ends_at)
-    "#{starts_at.strftime('%l%P')} - #{ends_at.strftime('%l%P')}"
+    "#{starts_at.strftime('%l%P')}–#{ends_at.strftime('%l%P')}"
   end
 end
