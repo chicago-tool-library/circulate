90411f54b07a89625bbf730b3a1e7a1fc3eb95f6
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-10-30 16:30:33 -0700

    Add test to check updates to borrow policy

--- test/controllers/admin/borrow_policies_controller_test.rb
+++ test/controllers/admin/borrow_policies_controller_test.rb
@@ -20,9 +20,19 @@ module Admin
       assert_response :success
     end
 
-    test "should update borrow_policy" do
+    test "should redirect to admin borrow policies page" do
       patch admin_borrow_policy_url(@borrow_policy), params: {borrow_policy: {duration: @borrow_policy.duration, fine_cents: @borrow_policy.fine_cents, fine_period: @borrow_policy.fine_period, name: @borrow_policy.name, code: "Q"}}
       assert_redirected_to admin_borrow_policies_url
+    end
+
+    test "should update borrow_policy" do
+      patch admin_borrow_policy_url(@borrow_policy), params: {borrow_policy: {duration: 10, fine: 8.23, fine_period: 26, name: "New name", code: "Q"}}
+      @borrow_policy.reload
+      assert_equal 10, @borrow_policy.duration
+      assert_equal 823, @borrow_policy.fine_cents
+      assert_equal 26, @borrow_policy.fine_period
+      assert_equal "New name", @borrow_policy.name
+      assert_equal "Q", @borrow_policy.code
     end
   end
 end
