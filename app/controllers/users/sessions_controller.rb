656e3b523090d73b71ec6a3ad330072eb406051c
Jim Benton <jim@autonomousmachine.com>
2020-11-06 22:32:57 -0600

    Don't create a user when storing their previous location.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,8 +4,7 @@ module Users
       if request.referer.present?
         referer = URI.parse(request.referer)
         if referer.host == request.host
-          self.resource = resource_class.create(sign_in_params)
-          store_location_for(resource, request.referer)
+          store_location_for(:user, request.referer)
         end
       end
     rescue URI::InvalidURIError
