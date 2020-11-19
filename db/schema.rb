bcd48c0a0b1203ab1525c0ef9bdefa6d3e5d08f1
Jim Benton <jim@autonomousmachine.com>
2020-11-18 18:44:26 -0600

    Migrate item manuals to item attachments.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_012007) do
+ActiveRecord::Schema.define(version: 2020_11_18_221831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
