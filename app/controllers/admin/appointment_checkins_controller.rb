64c7398025a2ac01f5887ddd5b3224142926581d
Jim Benton <jim@autonomousmachine.com>
2021-01-03 11:38:09 -0600

    Simplify Admin::AppointmentCheckinsController

--- app/controllers/admin/appointment_checkins_controller.rb
+++ app/controllers/admin/appointment_checkins_controller.rb
@@ -1,33 +1,17 @@
 module Admin
   class AppointmentCheckinsController < BaseController
     def create
-      return_loaned_items
-      redirect_to admin_appointment_path(appointment), flash: {success: "#{pluralize(returned_item_count, "Item")} checked-in."}
-    end
+      @appointment = Appointment.find(params[:appointment_id])
+      @loans = appointment.loans.checked_out.where(id: params[:loan_ids])
 
-    private
-
-    attr_accessor :returned_loans
-
-    delegate :pluralize, to: "ActionController::Base.helpers", private: true
-    delegate :member, to: :appointment
-
-    def returned_item_count
-      returned_loans&.count&.to_i
-    end
-
-    def loans
-      @loans ||= appointment.loans.checked_out.where(id: params[:loan_ids])
-    end
-
-    def appointment
-      @appointment ||= Appointment.find(params[:appointment_id])
-    end
-
-    def return_loaned_items
-      self.returned_loans = member.transaction do
-        loans.map(&:return!)
+      returned_loans = @appointment.member.transaction do
+        @loans.map(&:return!)
       end
+      returned_item_count = returned_loans&.count&.to_i
+
+      redirect_to admin_appointment_path(appointment), flash: {
+        success: "#{helpers.pluralize(returned_item_count, "Item")} checked-in."
+      }
     end
   end
 end
