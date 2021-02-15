612fce52c2058dd5b10a1ce6e4e5d78ca2f40ed0
Jim Benton <jim@autonomousmachine.com>
2021-02-15 14:45:53 -0600

    Fix issue where nulls were missing some methods.

---
+++ test/nulls/null_item_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class NullItemTest < ActiveSupport::TestCase
+  setup do
+    @item = NullItem.new
+  end
+
+  test "item API" do
+    @item.holds.active.count
+    @item.active?
+  end
+end
