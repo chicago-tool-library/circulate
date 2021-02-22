a4240d08c3dd256b0357c6bd5ad9e552b9dea715
Jim Benton <jim@autonomousmachine.com>
2021-02-22 10:38:47 -0600

    Update config to use new Sendgrid API key.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    user_name: ENV["SENDGRID_USERNAME"],
-    password: ENV["SENDGRID_PASSWORD"],
+    user_name: "apikey",
+    password: ENV["SENDGRID_API_KEY"],
     domain: "app.chicagotoollibrary.org",
     address: "smtp.sendgrid.net",
     port: 587,
