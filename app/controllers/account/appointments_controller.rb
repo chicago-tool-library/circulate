fe0dedd5e54a3932a2c72f6116ddf245573c5461
Stephanie Minn <stephanie.minn@madeintandem.com>
2021-03-23 14:00:58 -0500

    wip: add edit appointment functionality

--- app/controllers/account/appointments_controller.rb
+++ app/controllers/account/appointments_controller.rb
@@ -8,32 +8,52 @@ module Account
       @appointment = Appointment.new
       @holds = Hold.active.includes(member: {appointments: :holds}).where(member: current_user.member)
       @loans = current_user.member.loans.includes(:item, member: {appointments: :loans}).checked_out
-
+      @url = account_appointments_path
+      @method = :post
       load_appointment_slots
     end
 
     def create
       member = current_user.member
       @appointment = Appointment.new
-      @appointment.comment = appointment_params[:comment]
-      @appointment.member = member
-      @appointment.holds << Hold.where(id: appointment_params[:hold_ids], member: member)
-      @appointment.loans << Loan.where(id: appointment_params[:loan_ids], member: member)
 
-      if appointment_params[:time_range_string].present?
-        @appointment.time_range_string = appointment_params[:time_range_string]
-        appointment_times = appointment_params[:time_range_string].split("..")
-        @appointment.starts_at = DateTime.parse appointment_times[0]
-        @appointment.ends_at = DateTime.parse appointment_times[1]
-      end
-
-      if @appointment.save
+      if @appointment.update(appointment_fields)
         redirect_to account_appointments_path, notice: "Appointment was successfully created."
       else
-        @holds = Hold.active.where(member: current_user.member)
-        @loans = current_user.member.loans.includes(:item).checked_out
+        @holds = Hold.active.where(member: member)
+        @loans = member.loans.includes(:item).checked_out
+        @url = account_appointments_path
+        @method = :post
+
         load_appointment_slots
         render :new, alert: @appointment.errors.full_messages
+      end
+    end
+
+    def edit
+      @appointment = current_user.member.appointments.find(params[:id])
+      @holds = Hold.active.includes(member: {appointments: :holds}).where(member: current_user.member)
+      @loans = current_user.member.loans.includes(:item, member: {appointments: :loans}).checked_out
+      @comment = @appointment.comment
+      @url = account_appointment_path(@appointment)
+      @method = :put
+      @appointment.time_range_string = @appointment.starts_at.to_s + ".." + @appointment.ends_at.to_s
+      load_appointment_slots
+    end
+
+    def update
+      member = current_user.member
+      @appointment = member.appointments.find(params[:id])
+
+      if @appointment.update(appointment_fields)
+        redirect_to account_appointments_path, notice: "Appointment was successfully updated."
+      else
+        @holds = Hold.active.where(member: member)
+        @loans = member.loans.includes(:item).checked_out
+        @url = account_appointment_path(@appointment)
+        @method = :put
+        load_appointment_slots
+        render :edit, alert: @appointment.errors.full_messages
       end
     end
 
@@ -46,6 +66,40 @@ module Account
 
     def appointment_params
       params.require(:appointment).permit(:comment, :time_range_string, hold_ids: [], loan_ids: [])
+    end
+
+    def appointment_fields
+      member = current_user.member
+
+      if appointment_params[:time_range_string].present?
+        time_range_string = appointment_params[:time_range_string]
+        appointment_times = time_range_string.split("..")
+        starts_at = DateTime.parse appointment_times[0]
+        ends_at = DateTime.parse appointment_times[1]
+      end
+
+      {
+        member: member,
+        holds: Hold.where(id: appointment_params[:hold_ids], member: member),
+        loans: Loan.where(id: appointment_params[:loan_ids], member: member),
+        comment: appointment_params[:comment],
+        time_range_string: time_range_string,
+        starts_at: starts_at,
+        ends_at: ends_at
+      }
+    end
+
+    def update_appointment(appointment, url, method)
+      if appointment.update(appointment_fields)
+        redirect_to account_appointments_path, notice: "Appointment was successfully saved."
+      else
+        @holds = Hold.active.where(member: current_user.member)
+        @loans = current_user.member.loans.includes(:item).checked_out
+        @url = url
+        @method = method
+        load_appointment_slots
+        render :new, alert: appointment.errors.full_messages
+      end
     end
 
     def load_appointment_slots
