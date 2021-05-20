d5f328ada7bd70e49ce1406f69a88c8c623a0507
Jim Benton <jim@autonomousmachine.com>
2021-05-19 21:08:38 -0500

    Allow staff to mark appointments as complete.

---
+++ test/system/admin/appointments_test.rb
@@ -0,0 +1,36 @@
+require "application_system_test_case"
+
+module Admin
+  class AppointmentsTest < ApplicationSystemTestCase
+    include AdminHelper
+    include ActionView::RecordIdentifier
+
+    setup do
+      sign_in_as_admin
+    end
+
+    test "marks an appointment as completed" do
+      hold = create(:hold)
+      appointment = create(:appointment, holds: [hold], member: hold.member)
+
+      visit appointment_in_schedule_path(appointment)
+
+      id = "#" + dom_id(appointment)
+      within id do
+        click_on "complete"
+      end
+
+      assert_selector("#{id}.completed")
+
+      assert appointment.reload.completed_at.present?
+
+      within id do
+        click_on "restore"
+      end
+
+      refute_selector("#{id}.completed")
+
+      assert appointment.reload.completed_at.nil?
+    end
+  end
+end
