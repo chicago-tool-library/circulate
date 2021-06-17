96cdc21d11720c9b59f5f433802399311860fdf1
Jim Benton <jim@autonomousmachine.com>
2021-06-17 16:02:10 -0500

    Remove limit on how many volunteers can sign up

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,2 +1,17 @@
 module ShiftsHelper
+  def each_shift(events, &block)
+    last_date = nil
+    events.group_by { |e| e.start.to_date }.each do |date, daily_events|
+      daily_events.group_by { |e| [e.start, e.finish] }.each_with_index do |((start, finish), shift_events), shift_index|
+        shift_attendees_count = shift_events.inject(0) { |sum, de| de.accepted_attendees_count + sum }
+
+        shift_events.sort_by { |e| e.start }.each_with_index do |event, event_index|
+          daily_events_size = daily_events.size if last_date != date
+          shift_events_size = shift_events.size if event_index == 0
+          yield date, event, daily_events_size, shift_events_size, shift_attendees_count
+          last_date = date
+        end
+      end
+    end
+  end
 end
