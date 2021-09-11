403e1763a9205c5fad060e92f6928cf39853063e
pjsier <pjsier@gmail.com>
2021-09-10 20:57:32 -0500

    refactor: update to 6.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,24 @@ Bundler.require(*Rails.groups)
 module Circulate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
     # config.autoloader = :classic
+
+    config.active_record.has_many_inversing = false
+    config.active_record.legacy_connection_handling = true
+
+    config.active_storage.track_variants = false
+    config.active_storage.queues.analysis = :active_storage_analysis
 
     # config.active_storage.variant_processor = :vips
     config.active_job.queue_adapter = :sucker_punch
+    config.active_job.skip_after_callbacks_if_terminated = true
+
+    config.action_dispatch.cookies_same_site_protection = nil
+
+    config.action_view.form_with_generates_remote_forms = true
+
+    ActiveSupport.utc_to_local_returns_utc_offset_times = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
