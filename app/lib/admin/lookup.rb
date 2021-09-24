fd562cdc7fcfe0e33979a0e557e11a6e24674e51
rwyatt@taphere.com <rwyatt@taphere.com>
2021-09-24 15:15:06 -0400

    - Strip non-number characters from form entry - Add more specific error for invalid format

--- app/lib/admin/lookup.rb
+++ app/lib/admin/lookup.rb
@@ -6,12 +6,16 @@ module Admin
     attr_reader :item_number
 
     validates_each :item do |record, attr, value|
+      unless record.item_number.scan(/\D/).empty?
+        record.errors.add(:item_number, "Please enter a 1-4 digit number")
+      end
       unless value
-        record.errors.add(:item_number, "no item with that number")
+        record.errors.add(:item_number, "No item with that number")
       end
     end
 
     def item_number=(item_number)
+      item_number.gsub(/\D+/, "")
       @item_number = item_number
       @item = Item.find_by(number: item_number)
     end
