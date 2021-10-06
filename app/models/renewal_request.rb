0958ca464eabac31e8f4ba272f08bc4cf93bc0a9
Jim Benton <jim@autonomousmachine.com>
2021-10-06 14:43:43 -0500

    Remove problematic association from RenewalRequest

--- app/models/renewal_request.rb
+++ app/models/renewal_request.rb
@@ -1,6 +1,5 @@
 class RenewalRequest < ApplicationRecord
   belongs_to :loan
-  belongs_to :loan_summary, foreign_key: "loan_id", primary_key: "initial_loan_id", optional: true
 
   enum status: {
     requested: "requested",
