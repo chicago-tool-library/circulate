2cca167eadaa3648b085353758e5bf9ba3b8b87a
Kyle Fadigan <kyle.fadigan@powerhrg.com>
2021-10-12 15:09:41 -0400

    Add a flash message

--- app/controllers/admin/manage_features_controller.rb
+++ app/controllers/admin/manage_features_controller.rb
@@ -6,6 +6,7 @@ module Admin
 
     def update
       Library.find(@current_library.id).update_column :allow_members, params[:library][:allow_members]
+      redirect_to admin_manage_features_url, success: "You have successfully updated the library"
     end
   end
 end
