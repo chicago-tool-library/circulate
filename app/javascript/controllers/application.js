f2de07638a78b3d78a52d200151e7f9d659e2cc5
derrick lannaman <derrick.lannaman@powerhrg.com>
2021-10-13 12:01:35 -0400

    Add application.js for stimulus

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -3,6 +3,7 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 // Configure Stimulus development experience
+application.warnings = true
 application.debug = false
 window.Stimulus   = application
 
