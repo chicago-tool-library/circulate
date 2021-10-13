53f39e0912f3843fa767a118e5d06f77816e99b5
Ivan Candido <ivan.candido@powerhrg.com>
2021-10-13 16:32:25 -0400

    Refactored update params

--- app/controllers/admin/manage_features_controller.rb
+++ app/controllers/admin/manage_features_controller.rb
@@ -1,5 +1,6 @@
 module Admin
   class ManageFeaturesController < BaseController
+
     def index
       @current_library = current_library
     end
@@ -8,11 +9,17 @@ module Admin
       checked_value = {"0": false, "1": true}
 
       if checked_value[params[:library][:allow_members].to_sym] != @current_library.allow_members
-        Library.find(@current_library.id).update_column :allow_members, params[:library][:allow_members]
+        Library.find(@current_library.id).update(library_params)
         redirect_to admin_manage_features_url, success: "You have successfully updated the library"
       else
         redirect_to admin_manage_features_url, warning: "You didn't make any changes"
       end
+    end
+
+  private
+
+    def library_params
+      params.require(:library).permit(:allow_members, :allow_volunteers)
     end
   end
 end
