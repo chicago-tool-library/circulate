2a296739d47bc1d2da58afab080148d1a337b661
Kyle Fadigan <kyle.fadigan@powerhrg.com>
2021-10-14 11:46:17 -0400

    Refactor endpoint again

--- app/controllers/admin/manage_features_controller.rb
+++ app/controllers/admin/manage_features_controller.rb
@@ -5,24 +5,26 @@ module Admin
     end
 
     def update
-      checked_value = {"0": false, "1": true}
-      membership_param = params[:library][:allow_members]
-      payments_params = params[:library][:allow_payments]
-      membership_changed = checked_value[params[:library][:allow_members].to_sym] != @current_library.allow_members
-      payments_changed = checked_value[params[:library][:allow_payments].to_sym] != @current_library.allow_payments
-
-      if membership_changed
-        Library.find(@current_library.id).update_column :allow_members, membership_param
-      end
-      if payments_changed
-        Library.find(@current_library.id).update_column :allow_payments, payments_params
-      end
-
-      if membership_changed || payments_changed
+      if check_for_edit == true
+        Library.find(@current_library.id).update(library_feature_params)
         redirect_to admin_manage_features_url, success: "You have successfully updated the library"
       else
         redirect_to admin_manage_features_url, warning: "You didn't make any changes"
       end
+    end
+
+    private
+
+    def check_for_edit
+      checked_value = {"0": false, "1": true}
+
+      library_feature_params.keys.each do |feature|
+        return true if checked_value[library_feature_params[feature].to_sym] != @current_library[feature]
+      end
+    end
+
+    def library_feature_params
+      params.require(:library).permit(:allow_members, :allow_payments)
     end
   end
 end
