20c2c2af37f355463949230fc549d8acdc11c0e9
Jim Benton <jim@autonomousmachine.com>
2022-02-15 00:20:16 -0600

    Add additional item statuses.

---
+++ db/migrate/20220215035541_add_additional_item_statuses.rb
@@ -0,0 +1,6 @@
+class AddAdditionalItemStatuses < ActiveRecord::Migration[6.1]
+  def change
+    add_enum_value :item_status, "maintenance_repairing"
+    add_enum_value :item_status, "maintenance_parts"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_214711) do
+ActiveRecord::Schema.define(version: 2022_02_15_035541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,9 @@ ActiveRecord::Schema.define(version: 2022_02_02_214711) do
     "pending",
     "active",
     "maintenance",
-    "retired"
+    "retired",
+    "maintenance_repairing",
+    "maintenance_parts"
   ], force: :cascade
 
   create_enum :notification_status, [
