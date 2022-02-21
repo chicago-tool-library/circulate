b2e20f763b4cf5eed6578e18f12fac95d95f33d4
Jim Benton <jim@autonomousmachine.com>
2022-02-20 21:53:01 -0600

    Try class_eval

--- config/initializers/audited.rb
+++ config/initializers/audited.rb
@@ -1,6 +1,7 @@
 Rails.application.config.after_initialize do
   # this is gross, but I was running into issues using a custom Audit class
-  class Audited::Audit # standard:disable Lint/ConstantDefinitionInBlock
+
+  Audited::Audit.class_eval do
     has_one :maintenance_report, foreign_key: "audit_id"
   end
 end
