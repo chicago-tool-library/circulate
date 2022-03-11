4e744b363164223086b731defd92795e83d7c0ff
Stephanie Minn <12875392+stephanieminn@users.noreply.github.com>
2022-03-11 16:57:45 -0600

    Allow admins to view previous holds
    https://github.com/rubyforgood/circulate/issues/980
    
    Adds grouping buttons for admins to toggle between viewing a list of an item's active or inactive holds.

--- app/controllers/admin/item_holds_controller.rb
+++ app/controllers/admin/item_holds_controller.rb
@@ -2,7 +2,13 @@ module Admin
   class ItemHoldsController < BaseController
     def index
       @item = Item.find(params[:item_id])
-      @holds = @item.active_holds.order(created_at: :asc)
+      @holds = @item.holds.order(created_at: :asc)
+      @holds =
+        if params[:inactive]
+          @holds.inactive
+        else
+          @holds.active
+        end
     end
   end
 end
