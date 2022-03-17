6b2e1d2467079609a7324d015e8bed312d7fab86
Jim Benton <jim@autonomousmachine.com>
2022-03-17 16:54:13 -0500

    Remove extra pg extension

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_03_04_025546) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_enum :adjustment_kind, [
