6b12ca32d451997974fd7c7a55a01019572f3f2a
Jim Benton <jim@autonomousmachine.com>
2022-04-26 20:17:12 -0500

    Restore callbacks to refresh CategoryNodes

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -3,4 +3,13 @@ class Categorization < ApplicationRecord
   belongs_to :category, counter_cache: true
   belongs_to :category_node, foreign_key: "category_id"
   validates :category_id, uniqueness: {scope: :item_id}
+
+  after_save :refresh_category_nodes
+  after_destroy :refresh_category_nodes
+
+  private
+
+  def refresh_category_nodes
+    CategoryNode.refresh
+  end
 end
