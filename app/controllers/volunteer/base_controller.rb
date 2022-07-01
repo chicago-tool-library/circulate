bc4583d700bc1c8137bc9fd8b0969aa6af081682
Jim Benton <jim@autonomousmachine.com>
2022-07-01 16:12:42 -0500

    Redirect users to teamup for volunteer signups

--- app/controllers/volunteer/base_controller.rb
+++ app/controllers/volunteer/base_controller.rb
@@ -1,11 +1,17 @@
 module Volunteer
   class BaseController < ApplicationController
+    before_action :redirect_to_teamup
     helper_method :signed_in_via_google?
 
     private
 
     def signed_in_via_google?
       session[:email].present?
+    end
+
+    def redirect_to_teamup
+      redirect_to "https://teamup.com/ksnp8rrsst1ow8nvwn"
+      false
     end
   end
 end
