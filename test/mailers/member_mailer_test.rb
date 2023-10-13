5e0f18326e3c7bb2b0cf35a260e8da2377702a10
Paul Hinze <phinze@phinze.com>
2023-10-13 18:30:48 -0500

    Add note about adding library email to contacts to welcome messages

--- test/mailers/member_mailer_test.rb
+++ test/mailers/member_mailer_test.rb
@@ -31,4 +31,51 @@ class MemberMailerTest < ActionMailer::TestCase
       end
     end
   end
+
+  test "can deliver welcome email" do
+    member = create(:member)
+    email = MemberMailer.with(member: member).welcome_message
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+  end
+
+  test "welcome email only renders footer once" do
+    member = create(:member)
+    email = MemberMailer.with(member: member).welcome_message
+
+    footer_regexp = /See you at the library!/
+
+    assert_equal 1, email.text_part.body.to_s.scan(footer_regexp).size
+    assert_equal 1, email.html_part.body.to_s.scan(footer_regexp).size
+  end
+
+  test "welcome email recommends adding sender to contacts" do
+    member = create(:member)
+    email = MemberMailer.with(member: member).welcome_message
+
+    add_to_contacts_regexp = /Please consider adding.*#{member.library.email}.*to your contacts/
+
+    assert_match add_to_contacts_regexp, email.text_part.body.to_s
+    assert_match add_to_contacts_regexp, email.html_part.body.to_s
+  end
+
+  test "can deliver membership reminder email" do
+    member = create(:member)
+    email = MemberMailer.with(member: member).membership_reminder
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+  end
+
+  test "can deliver renewal email" do
+    member = create(:member)
+    email = MemberMailer.with(member: member).renewal_message
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+  end
 end
