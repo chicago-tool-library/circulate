b47920d218e773f17ce1c2758180fafada255ed3
Jim Benton <jim@autonomousmachine.com>
2023-10-29 17:14:51 -0500

    Add/extend tests for hold position editing.

---
+++ test/controllers/admin/items/holds/positions_controller_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+module Admin
+  module Items
+    module Holds
+      class PositionsControllerTest < ActionDispatch::IntegrationTest
+        include Devise::Test::IntegrationHelpers
+
+        setup do
+          @user = create(:admin_user)
+          create(:verified_member, user: @user)
+
+          sign_in @user
+        end
+
+        test "updates positions of holds" do
+          item = create(:item)
+          10.times.map { create(:hold, item: item) }
+          holds = item.holds.ordered_by_position
+          hold_ids = holds.map(&:id)
+
+          patch admin_item_hold_position_path(item.id, hold_ids[7]), params: {position: holds[2].position}
+
+          assert_response :success
+
+          reordered_hold_ids = item.holds.ordered_by_position.map(&:id)
+          expected_hold_ids = hold_ids[0..1] + [hold_ids[7]] + hold_ids[2..6] + hold_ids[8..]
+
+          assert_equal expected_hold_ids, reordered_hold_ids
+        end
+      end
+    end
+  end
+end
