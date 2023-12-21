2f03a8b85d65ee9c4f05ceb870bd0c3e4a2bfae7
Jim Benton <jim@autonomousmachine.com>
2023-12-20 19:46:45 -0600

    Remove limit from next_number

--- app/models/item.rb
+++ app/models/item.rb
@@ -104,11 +104,8 @@ class Item < ApplicationRecord
   before_save :cache_description_as_plain_text
   after_update :clear_holds_if_inactive, :pause_next_hold_if_maintenance
 
-  def self.next_number(limit = nil)
+  def self.next_number
     item_scope = order("number DESC NULLS LAST")
-    if limit
-      item_scope = item_scope.where("number <= ?", limit)
-    end
     last_item = item_scope.limit(1).first
     return 1 unless last_item
     last_item.number.to_i + 1
