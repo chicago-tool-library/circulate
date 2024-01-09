47ab0e4e78cf38f511e2f0fd8fb9fc80d7494f97
Jim Benton <3331+jim@users.noreply.github.com>
2024-01-08 21:24:37 -0600

    Remove special case for numbering A tools. Fixes #1223. (#1224)
    # What it does
    
    We originally treated A tools (those that we don't individually number)
    specially, with the idea being that having their numbers be 3 digits
    would be another signal that they were A tools. This worked, but...
    we're gonna run out of numbers since B tools start at 1000.
    
    So the solution is to remove that special case for A tools. Once this is
    merged, all new items will get the next available number in the system.
    
    # Why it is important
    
    I don't know what will happen if we hit 1000 for A tools, but let's not
    find out.
    
    # Your bandwidth for additional changes to this PR
    
    _Please choose one of the following to help the project maintainers
    provide the appropriate level of support:_
    
    - [x] I have the time and interest to make additional changes to this PR
    based on feedback.
    - [ ] I am interested in feedback but don't need to make the changes
    myself.
    - [ ] I don't have time or interest in making additional changes to this
    work.
    - [ ] Other or not sure (please describe):

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -11,6 +11,26 @@ class ItemTest < ActiveSupport::TestCase
     assert item.number
   end
 
+  test "assigns the next available for B tools" do
+    create(:item, number: 999)
+
+    borrow_policy = create(:borrow_policy, code: "B")
+    item = build(:item, number: nil, borrow_policy: borrow_policy)
+    item.save!
+
+    assert_equal 1000, item.number
+  end
+
+  test "assigns the next available number for A tools" do
+    create(:item, number: 999)
+
+    borrow_policy = create(:borrow_policy, code: "A")
+    item = build(:item, number: nil, borrow_policy: borrow_policy)
+    item.save!
+
+    assert_equal 1000, item.number
+  end
+
   test "it has a due_on date" do
     loan = create(:loan, due_at: Date.tomorrow.end_of_day)
     loan.item.reload
