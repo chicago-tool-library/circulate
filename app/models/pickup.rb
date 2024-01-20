6ae936213a44d8e0d7f24d2344c6966a7766e8fa
Jim Benton <jim@autonomousmachine.com>
2024-01-20 17:42:03 -0600

    Fix up views and tweak to be compatible with turbo.

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -1,4 +1,10 @@
 class Pickup < ApplicationRecord
+  enum status: {
+    building: "building",
+    ready_for_pickup: "ready_for_pickup",
+    picked_up: "picked_up",
+    cancelled: "cancelled"
+  }
   belongs_to :creator, class_name: "User"
   belongs_to :reservation
 end
