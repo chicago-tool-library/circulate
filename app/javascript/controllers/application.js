ea2fb6d2434d0b8576b115d435bf3cffb66731b9
Jim Benton <jim@autonomousmachine.com>
2024-01-21 17:01:32 -0600

    Collect failures from turbo navigation and send them to appsignal.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,6 +5,20 @@ import { installErrorHandler } from "@appsignal/stimulus"
 const application = Application.start()
 installErrorHandler(appsignal, application)
 
+// Send an error to AppSignal when a turbo fetch fails
+window.addEventListener("turbo:fetch-request-error", (event) => {
+    appsignal.sendError(event.detail.error, (span) => {
+        const target = event.target.cloneNode(false)
+        span.setAction("turbo:fetch-request-error")
+        span.setNamespace("turbo")
+        span.setParams({
+            request_url: event.detail.request.url.toString(),
+            target: target.outerHTML,
+            submitter: event.detail.request.delegate.submitter.outerHTML,
+        })
+    })
+})
+
 // Configure Stimulus development experience
 application.warnings = true
 application.debug = false
--- app/javascript/lib/appsignal.js
+++ app/javascript/lib/appsignal.js
@@ -1,3 +1,10 @@
 import Appsignal from "@appsignal/javascript"
+import { plugin as pathDecoratorPlugin } from "@appsignal/plugin-path-decorator"
+import { plugin as breadcrumbsConsolePlugin } from "@appsignal/plugin-breadcrumbs-console"
+import { plugin as breadcrumbsNetworkPlugin } from "@appsignal/plugin-breadcrumbs-network"
  
 export const appsignal = new Appsignal({key: process.env.APPSIGNAL_FRONTEND_KEY})
+
+appsignal.use(pathDecoratorPlugin())
+appsignal.use(breadcrumbsConsolePlugin())
+appsignal.use(breadcrumbsNetworkPlugin())
