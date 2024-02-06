3ed7613c056f251c65ba1e70b5b40c330188a977
Paul Hinze <phinze@phinze.com>
2024-02-05 18:16:44 -0600

    Remove unused smooth-scroll-into-view package
    As discussed in https://github.com/chicago-tool-library/circulate/pull/1272

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,16 +12,3 @@ import { setupFeatherIcons } from "./lib/feather"
 
 document.documentElement.addEventListener("turbo:load", setupFeatherIcons);
 document.documentElement.addEventListener("turbo:frame-render", setupFeatherIcons);
-
-// import scrollIntoView from 'smooth-scroll-into-view-if-needed';
-// 
-// Turbo.ScrollManager.prototype.scrollToElement = function(element) {
-//   let classes = element.classList;
-//   if (classes.contains("highlightable")) {
-//     classes.add("highlight");
-//   }
-//   scrollIntoView(element, {
-//     behavior: 'smooth',
-//     scrollMode: 'if-needed',
-//   });
-// }
