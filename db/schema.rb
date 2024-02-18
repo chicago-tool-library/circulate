5b5bfa4ad5764e12f1b38600e3b2c6269e4c4c0a
Jim Benton <jim@autonomousmachine.com>
2024-02-17 19:04:24 -0600

    Fix time precision drift from main

--- db/schema.rb
+++ db/schema.rb
@@ -103,7 +103,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -115,7 +115,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
     t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum"
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
@@ -167,13 +167,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
   end
 
   create_table "appointments", force: :cascade do |t|
-    t.datetime "starts_at", null: false
-    t.datetime "ends_at", null: false
+    t.datetime "starts_at", precision: nil, null: false
+    t.datetime "ends_at", precision: nil, null: false
     t.text "comment", default: "", null: false
     t.bigint "member_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "completed_at"
+    t.datetime "completed_at", precision: nil
     t.index ["member_id"], name: "index_appointments_on_member_id"
   end
 
@@ -191,7 +191,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
     t.string "comment"
     t.string "remote_address"
     t.string "request_uuid"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.index ["associated_type", "associated_id"], name: "associated_index"
     t.index ["auditable_type", "auditable_id", "version"], name: "auditable_index"
     t.index ["created_at"], name: "index_audits_on_created_at"
@@ -264,8 +264,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
   create_table "events", force: :cascade do |t|
     t.string "calendar_id", null: false
     t.string "calendar_event_id", null: false
-    t.datetime "start", null: false
-    t.datetime "finish", null: false
+    t.datetime "start", precision: nil, null: false
+    t.datetime "finish", precision: nil, null: false
     t.string "summary"
     t.string "description"
     t.datetime "created_at", null: false
@@ -296,11 +296,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
     t.bigint "creator_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "ended_at"
+    t.datetime "ended_at", precision: nil
     t.bigint "loan_id"
     t.integer "library_id"
-    t.datetime "started_at"
-    t.datetime "expires_at"
+    t.datetime "started_at", precision: nil
+    t.datetime "expires_at", precision: nil
     t.integer "position", null: false
     t.index ["creator_id"], name: "index_holds_on_creator_id"
     t.index ["item_id", "position"], name: "index_holds_on_item_id_and_position", unique: true
@@ -394,8 +394,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
   create_table "loans", force: :cascade do |t|
     t.bigint "item_id"
     t.bigint "member_id"
-    t.datetime "due_at"
-    t.datetime "ended_at"
+    t.datetime "due_at", precision: nil
+    t.datetime "ended_at", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "uniquely_numbered", null: false
@@ -565,16 +565,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
-    t.datetime "locked_at"
+    t.datetime "locked_at", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.enum "role", default: "member", null: false, enum_type: "user_role"
@@ -642,8 +642,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
     GROUP BY loans.library_id, loans.item_id, loans.member_id, COALESCE(loans.initial_loan_id, loans.id);
   SQL
   create_view "monthly_adjustments", sql_definition: <<-SQL
-      SELECT (date_part('year'::text, adjustments.created_at))::integer AS year,
-      (date_part('month'::text, adjustments.created_at))::integer AS month,
+      SELECT (EXTRACT(year FROM adjustments.created_at))::integer AS year,
+      (EXTRACT(month FROM adjustments.created_at))::integer AS month,
       count(*) FILTER (WHERE ((adjustments.kind = 'membership'::adjustment_kind) AND (adjustments.adjustable_id = first_memberships.first_membership_id))) AS new_membership_count,
       sum((- adjustments.amount_cents)) FILTER (WHERE ((adjustments.kind = 'membership'::adjustment_kind) AND (adjustments.adjustable_id = first_memberships.first_membership_id))) AS new_membership_total_cents,
       count(*) FILTER (WHERE ((adjustments.kind = 'membership'::adjustment_kind) AND (adjustments.adjustable_id <> first_memberships.first_membership_id))) AS renewal_membership_count,
@@ -657,8 +657,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
              FROM (members
                LEFT JOIN memberships ON ((members.id = memberships.member_id)))
             GROUP BY members.id) first_memberships ON ((first_memberships.member_id = adjustments.member_id)))
-    GROUP BY ((date_part('year'::text, adjustments.created_at))::integer), ((date_part('month'::text, adjustments.created_at))::integer)
-    ORDER BY ((date_part('year'::text, adjustments.created_at))::integer), ((date_part('month'::text, adjustments.created_at))::integer);
+    GROUP BY ((EXTRACT(year FROM adjustments.created_at))::integer), ((EXTRACT(month FROM adjustments.created_at))::integer)
+    ORDER BY ((EXTRACT(year FROM adjustments.created_at))::integer), ((EXTRACT(month FROM adjustments.created_at))::integer);
   SQL
   create_view "monthly_loans", sql_definition: <<-SQL
       WITH dates AS (
@@ -669,8 +669,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
            SELECT generate_series(dates.startm, dates.endm, 'P1M'::interval) AS month
              FROM dates
           )
-   SELECT (date_part('year'::text, months.month))::integer AS year,
-      (date_part('month'::text, months.month))::integer AS month,
+   SELECT (EXTRACT(year FROM months.month))::integer AS year,
+      (EXTRACT(month FROM months.month))::integer AS month,
       count(DISTINCT l.id) AS loans_count,
       count(DISTINCT l.member_id) AS active_members_count
      FROM (months
@@ -687,8 +687,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
            SELECT generate_series(dates.startm, dates.endm, 'P1M'::interval) AS month
              FROM dates
           )
-   SELECT (date_part('year'::text, months.month))::integer AS year,
-      (date_part('month'::text, months.month))::integer AS month,
+   SELECT (EXTRACT(year FROM months.month))::integer AS year,
+      (EXTRACT(month FROM months.month))::integer AS month,
       count(DISTINCT m.id) FILTER (WHERE (m.status = 0)) AS pending_members_count,
       count(DISTINCT m.id) FILTER (WHERE (m.status = 1)) AS new_members_count
      FROM (months
@@ -705,8 +705,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_191129) do
            SELECT generate_series(dates.startm, dates.endm, 'P1M'::interval) AS month
              FROM dates
           )
-   SELECT (date_part('year'::text, months.month))::integer AS year,
-      (date_part('month'::text, months.month))::integer AS month,
+   SELECT (EXTRACT(year FROM months.month))::integer AS year,
+      (EXTRACT(month FROM months.month))::integer AS month,
       count(DISTINCT a.id) AS appointments_count,
       count(DISTINCT a.id) FILTER (WHERE (a.completed_at IS NOT NULL)) AS completed_appointments_count
      FROM (months
