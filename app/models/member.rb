650fb22f123fc21467ea46e8415c7941e59b2507
Jim Benton <jim@autonomousmachine.com>
2024-03-18 23:25:40 -0500

    Clean up member status column

--- app/models/member.rb
+++ app/models/member.rb
@@ -122,6 +122,18 @@ class Member < ApplicationRecord
     "+1#{phone_number}"
   end
 
+  def membership_status
+    if active_membership
+      "active"
+    elsif pending_membership
+      "pending"
+    elsif last_membership
+      "ended"
+    else
+      "no membership"
+    end
+  end
+
   private
 
   def update_user_email
