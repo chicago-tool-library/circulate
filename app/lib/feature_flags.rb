c60ac5a97788fa5029ac41e82d8d41a1d8682e48
Paul Hinze <phinze@phinze.com>
2024-03-20 11:44:38 -0500

    Convert new appointments page to feature flag (#1416)
    # What it does
    
    In our weekly meeting yesterday we reviewed the staff feedback on the
    new appointments page, and we decided the next best step is to pull the
    old page back and place the new look behind a feature flag to let us
    iterate on it.
    
    This feature flag is able to be set with an env var but also able to be
    overridden by a query param to allow easy toggling back and forth
    between old and new looks.
    
    # Why it is important
    
    This should allow us to iterate on updates to the page without
    interrupting the flow for librarians on active shifts.
    
    # UI Change Screenshot
    
    See #1290 for old and new screenshots.
    
    # Implementation notes
    
    * Separate view files seemed cleaner than conditionals inside the view
    code
    * Contents of index_orig and _appointment_orig pulled from
    4f58e70808e021639e0588499fb9bed68cdc9b89, the parent commit of #1290's
    merge

--- app/lib/feature_flags.rb
+++ app/lib/feature_flags.rb
@@ -2,4 +2,12 @@ class FeatureFlags
   def self.sms_reminders_enabled?
     ENV["FEATURE_SMS_REMINDERS"] == "on"
   end
+
+  def self.new_appointments_page_enabled?(override = nil)
+    if override.nil?
+      ENV["FEATURE_NEW_APPOINTMENTS_PAGE"] == "on"
+    else
+      ActiveModel::Type::Boolean.new.cast(override)
+    end
+  end
 end
