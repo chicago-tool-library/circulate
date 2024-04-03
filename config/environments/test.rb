b1ae84f9cceb68a96fd53d645c192a8ff0ebb245
Paul Hinze <phinze@phinze.com>
2024-04-03 11:26:34 -0500

    Fix CSRF error when reordering holds
    There was a small bug in getting the right token content from the meta
    tag which tests didn't catch because they have CSRF protection off.
    AFAICT there's no harm keeping CSRF on in tests so it covers issues like
    this going forward.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,9 +32,6 @@ Rails.application.configure do
   # Render exception templates for rescuable exceptions and raise for other exceptions.
   config.action_dispatch.show_exceptions = :rescuable
 
-  # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
-
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
 
