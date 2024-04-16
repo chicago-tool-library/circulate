f207da1f9a7d6327592ec80204449a3f6c226567
Paul Hinze <phinze@phinze.com>
2024-04-15 19:30:38 -0500

    Add Ahoy for tracking and begin recording events for tool search (#1454)
    # What it does
    
    This installs the [`ahoy_matey`](https://github.com/ankane/ahoy) gem for
    first-party analytics and begins using it to track when members do a
    search, view an item, and place a hold.
    
    # Why it is important
    
    As we aim to improve search in the system (#1240), it will be helpful to
    see what users are actually searching for and whether they are finding
    it.
    
    # Implementation notes
    
    * 🤫 By using a first-party analytics system (i.e. one that stores data
    in the same database we already use and control) we're not storing or
    exposing any more user information than we already have via our member
    information and logs.
    * 🏋🏻 It's worth noting that this will put more storage pressure on the
    database, because it's recording rows for stuff we expect users to do a
    lot. So we'll want to keep an eye on database growth.
    * ♻️ Along those lines, will want to add [a job that prunes old
    data](https://github.com/ankane/ahoy?tab=readme-ov-file#data-retention)
    but I figure that can come in a follow-on PR.
    * 📈 We can add as many more events as we like over time, but I figure
    starting relatively simple is the way to go.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_004211) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_11_151214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -151,6 +151,49 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_14_004211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["member_id"], name: "index_agreement_acceptances_on_member_id"
+  end
+
+  create_table "ahoy_events", force: :cascade do |t|
+    t.bigint "visit_id"
+    t.bigint "user_id"
+    t.string "name"
+    t.jsonb "properties"
+    t.datetime "time"
+    t.index ["name", "time"], name: "index_ahoy_events_on_name_and_time"
+    t.index ["properties"], name: "index_ahoy_events_on_properties", opclass: :jsonb_path_ops, using: :gin
+    t.index ["user_id"], name: "index_ahoy_events_on_user_id"
+    t.index ["visit_id"], name: "index_ahoy_events_on_visit_id"
+  end
+
+  create_table "ahoy_visits", force: :cascade do |t|
+    t.string "visit_token"
+    t.string "visitor_token"
+    t.bigint "user_id"
+    t.string "ip"
+    t.text "user_agent"
+    t.text "referrer"
+    t.string "referring_domain"
+    t.text "landing_page"
+    t.string "browser"
+    t.string "os"
+    t.string "device_type"
+    t.string "country"
+    t.string "region"
+    t.string "city"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "utm_source"
+    t.string "utm_medium"
+    t.string "utm_term"
+    t.string "utm_content"
+    t.string "utm_campaign"
+    t.string "app_version"
+    t.string "os_version"
+    t.string "platform"
+    t.datetime "started_at"
+    t.index ["user_id"], name: "index_ahoy_visits_on_user_id"
+    t.index ["visit_token"], name: "index_ahoy_visits_on_visit_token", unique: true
+    t.index ["visitor_token", "started_at"], name: "index_ahoy_visits_on_visitor_token_and_started_at"
   end
 
   create_table "appointment_holds", force: :cascade do |t|
