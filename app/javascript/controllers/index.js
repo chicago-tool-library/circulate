b5480eafd7c36e616da2a2a598c2940c393bf3dd
Katie Lentes <katielentes@noreply,github.com>
2024-05-19 21:19:02 -0500

    cleanup/linting

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,61 +5,63 @@
 import { application } from "./application"
 
 import AlertController from "./alert_controller"
-application.register("alert", AlertController)
 
 import AppointmentDateController from "./appointment_date_controller"
-application.register("appointment-date", AppointmentDateController)
 
 import AutocompleteController from "./autocomplete_controller"
-application.register("autocomplete", AutocompleteController)
 
 import CollapseController from "./collapse_controller"
-application.register("collapse", CollapseController)
 
 import ConditionalFieldController from "./conditional_field_controller"
-application.register("conditional-field", ConditionalFieldController)
 
 import DynamicFieldsController from "./dynamic_fields_controller"
-application.register("dynamic-fields", DynamicFieldsController)
 
 import EmailSettingsEditorController from "./email_settings_editor_controller"
-application.register("email-settings-editor", EmailSettingsEditorController)
 
 import FindToolController from "./find_tool_controller"
-application.register("find-tool", FindToolController)
 
 import HoldOrderController from "./hold_order_controller"
-application.register("hold-order", HoldOrderController)
 
 import ImageEditorController from "./image_editor_controller"
-application.register("image-editor", ImageEditorController)
 
 import ItemFilterController from "./item_filter_controller"
-application.register("item-filter", ItemFilterController)
 
 import ModalController from "./modal_controller"
-application.register("modal", ModalController)
 
 import MultiSelectController from "./multi_select_controller"
-application.register("multi-select", MultiSelectController)
 
 import NotesController from "./notes_controller"
-application.register("notes", NotesController)
 
 import RequestItemController from "./request_item_controller"
-application.register("request-item", RequestItemController)
 
 import ReservationDatesController from "./reservation_dates_controller"
-application.register("reservation-dates", ReservationDatesController)
 
 import SidebarController from "./sidebar_controller"
-application.register("sidebar", SidebarController)
 
 import TagEditorController from "./tag_editor_controller"
-application.register("tag-editor", TagEditorController)
 
 import ToggleController from "./toggle_controller"
-application.register("toggle", ToggleController)
 
 import TreeNavController from "./tree_nav_controller"
-application.register("tree-nav", TreeNavController)
+
+import TreeNavController from './tree_nav_controller'
+application.register('alert', AlertController)
+application.register('appointment-date', AppointmentDateController)
+application.register('autocomplete', AutocompleteController)
+application.register('collapse', CollapseController)
+application.register('conditional-field', ConditionalFieldController)
+application.register('dynamic-fields', DynamicFieldsController)
+application.register('email-settings-editor', EmailSettingsEditorController)
+application.register('find-tool', FindToolController)
+application.register('hold-order', HoldOrderController)
+application.register('image-editor', ImageEditorController)
+application.register('item-filter', ItemFilterController)
+application.register('modal', ModalController)
+application.register('multi-select', MultiSelectController)
+application.register('notes', NotesController)
+application.register('request-item', RequestItemController)
+application.register("reservation-dates", ReservationDatesController)
+application.register('sidebar', SidebarController)
+application.register('tag-editor', TagEditorController)
+application.register('toggle', ToggleController)
+application.register('tree-nav', TreeNavController)
