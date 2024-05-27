952e6d96cf1699a4be483c91b3ebba746dac2bdc
Katie Lentes <katielentes@noreply,github.com>
2024-05-26 22:49:52 -0500

    js linting

--- app/javascript/controllers/reservation_dates_controller.js
+++ app/javascript/controllers/reservation_dates_controller.js
@@ -1,19 +1,19 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ["startDate", "endDate"]
+  static targets = ['startDate', 'endDate']
 
-  connect() {
-    this.setMinEndDate();
-    this.startDateTarget.addEventListener('change', this.setMinEndDate.bind(this));
+  connect () {
+    this.setMinEndDate()
+    this.startDateTarget.addEventListener('change', this.setMinEndDate.bind(this))
   }
 
-  setMinEndDate() {
+  setMinEndDate () {
     if (this.startDateTarget.value) {
-      let dayAfterStartDate = new Date(this.startDateTarget.value);
-      dayAfterStartDate.setDate(dayAfterStartDate.getDate() + 1);
-      let minEndDate = dayAfterStartDate.toISOString().split('T')[0];
-      this.endDateTarget.min = minEndDate;
+      const dayAfterStartDate = new Date(this.startDateTarget.value)
+      dayAfterStartDate.setDate(dayAfterStartDate.getDate() + 1)
+      const minEndDate = dayAfterStartDate.toISOString().split('T')[0]
+      this.endDateTarget.min = minEndDate
     }
   }
 }
