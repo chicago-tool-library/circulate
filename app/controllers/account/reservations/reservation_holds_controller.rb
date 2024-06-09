612eec222bb66c460bffc7c8838c2786676e5fbd
Jim Benton <jim@autonomousmachine.com>
2024-06-08 20:34:14 -0500

    Members can request reservations. Note that the reservations aren't yet associated with that user!

---
+++ app/controllers/account/reservations/reservation_holds_controller.rb
@@ -0,0 +1,51 @@
+module Account
+  module Reservations
+    class ReservationHoldsController < BaseController
+      before_action :set_reservation_hold, except: :create
+
+      def show
+        render_turbo_response :show
+      end
+
+      def edit
+        render_turbo_response :edit
+      end
+
+      def update
+        if @reservation_hold.update(reservation_hold_params)
+          render_turbo_response :show
+        else
+          render_turbo_response :edit
+        end
+      end
+
+      def create
+        @reservation_hold = @reservation.reservation_holds.new(reservation_hold_params)
+
+        if @reservation_hold.save
+          redirect_to account_reservation_path(@reservation), status: :see_other
+        else
+          render_turbo_response :create_error
+        end
+      end
+
+      def destroy
+        if @reservation_hold.destroy
+          render_turbo_response :destroy
+        else
+          render_turbo_response :edit
+        end
+      end
+
+      private
+
+      def set_reservation_hold
+        @reservation_hold = @reservation.reservation_holds.find(params[:id])
+      end
+
+      def reservation_hold_params
+        params.require(:reservation_hold).permit(:quantity, :item_pool_id)
+      end
+    end
+  end
+end
---
+++ app/controllers/account/reservations/submissions_controller.rb
@@ -0,0 +1,29 @@
+module Account
+  module Reservations
+    class SubmissionsController < BaseController
+      before_action :set_reservation
+      before_action :check_reservation_status
+
+      def show
+      end
+
+      def update
+        # TODO add other validation here
+        @reservation.manager.request
+        if @reservation.save
+          redirect_to account_reservation_url(@reservation), success: "The reservation was submitted."
+        else
+          redirect_to account_reservation_url(@reservation), error: "This reservation couldn't be submitted."
+        end
+      end
+
+      private
+
+      def check_reservation_status
+        unless @reservation.manager.can?(:request)
+          redirect_to account_reservation_url(@reservation), error: "This reservation can't be submitted."
+        end
+      end
+    end
+  end
+end
