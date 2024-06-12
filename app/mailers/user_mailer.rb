e4240094df40c3343b181b834a4dde7a7dc4855e
Michael Crismali <michael@crismali.com>
2024-06-12 17:00:39 -0500

    Create Notifications whenever Devise sends an email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < Devise::Mailer
   default from: "Chicago Tool Library <team@chicagotoollibrary.org>"
-  after_action :store_notification, only: [:reset_password_instructions]
+  after_action :store_notification
 
   def reset_password_instructions(record, token, opts = {})
     @user = record
@@ -9,6 +9,12 @@ class UserMailer < Devise::Mailer
   end
 
   def store_notification
-    Notification.create!(member: @user.member, uuid: SecureRandom.uuid, action: action_name, address: @user.email, subject: @subject)
+    Notification.create!(
+      member: @user.member,
+      uuid: SecureRandom.uuid,
+      action: action_name,
+      address: @user.email,
+      subject: @subject.presence || mail.subject
+    )
   end
 end
