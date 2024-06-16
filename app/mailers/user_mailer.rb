0fe15b0ec7ecd32a65034bd9b46f26a1187ff432
Michael Crismali <michael@crismali.com>
2024-06-16 11:02:17 -0500

    Remove UserMailer#reset_password_instructions override and references to @subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,19 +2,13 @@ class UserMailer < Devise::Mailer
   default from: "Chicago Tool Library <team@chicagotoollibrary.org>"
   after_action :store_notification
 
-  def reset_password_instructions(record, token, opts = {})
-    @user = record
-    @subject = "You requested a password reset"
-    super
-  end
-
   def store_notification
     Notification.create!(
-      member: @user.member,
-      uuid: SecureRandom.uuid,
       action: action_name,
       address: @user.email,
-      subject: @subject.presence || mail.subject
+      member: @user.member,
+      subject: mail.subject,
+      uuid: SecureRandom.uuid
     )
   end
 end
