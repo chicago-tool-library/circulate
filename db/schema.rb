957a420aaaff506606b7c6a50a2fbd2d6d0d5171
Paul Hinze <phinze@phinze.com>
2024-06-27 09:42:28 -0500

    Install Blazer for custom analytics dashboards (#1567)
    # What it does
    
    * Installs the [Blazer](https://github.com/ankane/blazer) gem
    * Admins can now visit the Blazer dashboard at `/blazer`
    
    # Why it is important
    
    * Lets non-Heroku-admins arbitrarily query the database
    * Lets app admins create dashboards
    * Opens up ability for us to explore the data we're recording with Ahoy
    
    # UI Change Screenshot
    
    ![2024-06-25 at 11 30
    01@2x](https://github.com/chicago-tool-library/circulate/assets/37534/3d1557a8-c1ad-4519-8077-e4d122dfba79)
    
    # Implementation notes
    
    * Blazer recommends creating a readonly user for it to use, but our
    Heroku database plans do not allow us anything beyond the default user,
    so we'll have to rely on Blazer's transaction wrapping and our own
    carefulness to avoid losing data.
    * My plan is to set the environment variable for the DB addon attachment
    to BLAZER_DATABASE_URL with a command like this: `heroku addons:attach
    (dbname) -a (appname) --as BLAZER_DATABASE_URL`
    * Blazer has several other features controlled via its config file that
    we can explore once we've had a chance to play with it.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_17_233510) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_154800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -245,6 +245,62 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_17_233510) do
     t.index ["created_at"], name: "index_audits_on_created_at"
     t.index ["request_uuid"], name: "index_audits_on_request_uuid"
     t.index ["user_id", "user_type"], name: "user_index"
+  end
+
+  create_table "blazer_audits", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "query_id"
+    t.text "statement"
+    t.string "data_source"
+    t.datetime "created_at"
+    t.index ["query_id"], name: "index_blazer_audits_on_query_id"
+    t.index ["user_id"], name: "index_blazer_audits_on_user_id"
+  end
+
+  create_table "blazer_checks", force: :cascade do |t|
+    t.bigint "creator_id"
+    t.bigint "query_id"
+    t.string "state"
+    t.string "schedule"
+    t.text "emails"
+    t.text "slack_channels"
+    t.string "check_type"
+    t.text "message"
+    t.datetime "last_run_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_blazer_checks_on_creator_id"
+    t.index ["query_id"], name: "index_blazer_checks_on_query_id"
+  end
+
+  create_table "blazer_dashboard_queries", force: :cascade do |t|
+    t.bigint "dashboard_id"
+    t.bigint "query_id"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dashboard_id"], name: "index_blazer_dashboard_queries_on_dashboard_id"
+    t.index ["query_id"], name: "index_blazer_dashboard_queries_on_query_id"
+  end
+
+  create_table "blazer_dashboards", force: :cascade do |t|
+    t.bigint "creator_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_blazer_dashboards_on_creator_id"
+  end
+
+  create_table "blazer_queries", force: :cascade do |t|
+    t.bigint "creator_id"
+    t.string "name"
+    t.text "description"
+    t.text "statement"
+    t.string "data_source"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_blazer_queries_on_creator_id"
   end
 
   create_table "borrow_policies", force: :cascade do |t|
