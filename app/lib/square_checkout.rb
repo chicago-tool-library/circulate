3e2fe68803ccf774030af657603be6f3d3c9d244
Jim Benton <jim@autonomousmachine.com>
2024-06-27 23:43:23 -0500

    Move Square payment processing into a background job.

--- app/lib/square_checkout.rb
+++ app/lib/square_checkout.rb
@@ -1,6 +1,52 @@
 class SquareCheckout
+  class SquareError < StandardError; end
+
+  class FetchedOrder
+    def initialize(payload)
+      @payload = payload
+    end
+
+    def id
+      @payload[:id]
+    end
+
+    def state
+      @payload[:state]
+    end
+
+    def version
+      @payload[:version]
+    end
+
+    def fulfillment
+      # Orders created by the Payment link API only have a single fulfillment
+      @payload[:fulfillments][0]
+    end
+
+    def amount
+      @amount ||= extract_amount
+    end
+
+    def member_id
+      @payload[:metadata][:member_id]&.to_i
+    end
+
+    def created_by_circulate?
+      @payload[:metadata][:created_by] == "circulate"
+    end
+
+    private
+
+    def extract_amount
+      amount_money = @payload[:tenders][0][:amount_money]
+      raise "non-USD currency is not supported" unless amount_money[:currency] == "USD"
+      Money.new(amount_money[:amount])
+    end
+  end
+
   def initialize(access_token:, location_id:, environment: "production", now: Time.current)
-    @client = Square::Client.new(access_token: access_token, environment: environment)
+    bearer_auth_credential = Square::BearerAuthCredentials.new(access_token: access_token)
+    @client = Square::Client.new(bearer_auth_credentials: bearer_auth_credential, environment: environment)
     @location_id = location_id
     @now = now
   end
@@ -34,8 +80,12 @@ class SquareCheckout
               amount: amount.cents,
               currency: "USD"
             }
+          }],
 
-          }]
+          metadata: {
+            member_id: member_id.to_s,
+            created_by: "circulate"
+          }
         },
         payment_note: "Chicago Tool Library annual membership"
       }
@@ -43,25 +93,44 @@ class SquareCheckout
     if checkout_response.success?
       Result.success(checkout_response.body.payment_link[:url])
     else
-      Result.failure(checkout_response.errors)
+      Result.failure(SquareError.new("failed to create payment link: #{checkout_response.errors}"))
     end
+  end
+
+  def complete_order(fetched_order)
+    # Square requires that all fulfillments be marked as completed when setting the order's
+    # state to completed.
+    fulfillment = fetched_order.fulfillment.dup
+    fulfillment[:state] = "COMPLETED"
+
+    @client.orders.update_order(
+      order_id: fetched_order.id,
+      body: {
+        order: {
+          version: fetched_order.version,
+          state: "COMPLETED",
+          fulfillments: [fulfillment]
+        }
+      }
+    )
   end
 
   def fetch_order(order_id:)
     order_response = @client.orders.retrieve_order(order_id: order_id)
 
     if order_response.success?
-      order = order_response.body.order
-      amount_money = order[:tenders][0][:amount_money]
-
-      raise "non-USD currency is not supported" unless amount_money[:currency] == "USD"
-
-      amount = Money.new(amount_money[:amount])
-
-      Result.success(amount)
+      Result.success(FetchedOrder.new(order_response.body.order))
     else
       Result.failure(order_response.errors)
     end
+  end
+
+  def self.from_env
+    SquareCheckout.new(
+      access_token: ENV.fetch("SQUARE_ACCESS_TOKEN"),
+      location_id: ENV.fetch("SQUARE_LOCATION_ID"),
+      environment: ENV.fetch("SQUARE_ENVIRONMENT")
+    )
   end
 
   private
---
+++ test/controllers/square_controller_test.rb
@@ -0,0 +1,71 @@
+require "test_helper"
+
+class SquareControllerTest < ActionDispatch::IntegrationTest
+  def payment_updated_payload
+    {merchant_id: "HE0AV82NDSPXY",
+     type: "payment.updated",
+     event_id: "71c54eb8-f70a-3ef5-aebd-6093457e6f32",
+     created_at: "2024-06-25T05:40:17.581Z",
+     data: {
+       type: "payment",
+       id: "jDLRuBLA2KPl7JYORsmJhxHKUvGZY",
+       object: {
+         payment: {
+           amount_money: {
+             amount: 5500,
+             currency: "USD"
+           },
+           application_details: {
+             application_id: "sandbox-sq0idb-lky4CaPAWmDnHY3YtYxINg",
+             square_product: "ECOMMERCE_API"
+           },
+           capabilities: [
+             "EDIT_AMOUNT_UP",
+             "EDIT_AMOUNT_DOWN",
+             "EDIT_TIP_AMOUNT_UP",
+             "EDIT_TIP_AMOUNT_DOWN"
+           ],
+           created_at: "2024-06-25T05:40:16.194Z",
+           external_details: {
+             source: "Developer Control Panel",
+             type: "CARD"
+           },
+           id: "jDLRuBLA2KPl7JYORsmJhxHKUvGZY",
+           location_id: "X9QSBEAF8RK29",
+           note: "Chicago Tool Library annual membership",
+           order_id: "bfE82Bs4tWAEyTrcoxIpVl42Qe4F",
+           receipt_number: "jDLR",
+           receipt_url: "https://squareupsandbox.com/receipt/preview/jDLRuBLA2KPl7JYORsmJhxHKUvGZY",
+           source_type: "EXTERNAL",
+           status: "COMPLETED",
+           total_money: {
+             amount: 5500,
+             currency: "USD"
+           },
+           updated_at: "2024-06-25T05:40:16.335Z",
+           version: 1
+         }
+       }
+     }}
+  end
+
+  test "rejects requests that aren't signed correctly" do
+    assert_no_enqueued_jobs do
+      post square_callback_url, params: payment_updated_payload, headers: {
+        "X-SQUARE-HMACSHA256-SIGNATURE": "invalid-sgnature"
+      }
+
+      assert_equal 401, response.status
+    end
+  end
+
+  test "handles payment updates" do
+    assert_enqueued_with(job: SquarePaymentJob, args: [{library_id: current_library.id, order_id: "bfE82Bs4tWAEyTrcoxIpVl42Qe4F"}]) do
+      post square_callback_url, params: payment_updated_payload, headers: {
+        "X-SQUARE-HMACSHA256-SIGNATURE": "s2TfTqnEVXKbXile3L/LfCd/MMhcQXRHQXROromxPDI="
+      }
+
+      assert response.ok?
+    end
+  end
+end
