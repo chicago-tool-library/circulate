9fad5dc119f2a65ac4a7009891d8d201a9e76640
Michael Crismali <michael@crismali.com>
2024-07-16 00:28:27 -0500

    Added "missing" to item statuses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_161420) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_051930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_10_161420) do
     "active",
     "maintenance",
     "retired",
+    "missing",
   ], force: :cascade
 
   create_enum :membership_type, [
