62d9b16d3e1e99bdb1095784ae7ece2d3f4b45be
djcunningham0 <djcunningham0@gmail.com>
2024-07-20 16:50:24 -0500

    handle edge case where filter_params doesn't exist; remove unnecessary condition

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
 
     # Track that a search was performed if we are on the first page
     if @query && @pagy.page == 1
-      ahoy.track "Searched", query: @query, params: @filter_params, n_results: @pagy.count, first_page_results: @items.map(&:id)
+      ahoy.track "Searched", query: @query, params: (@filter_params || {}), n_results: @pagy.count, first_page_results: @items.map(&:id)
     end
   end
 
@@ -36,7 +36,7 @@ class ItemsController < ApplicationController
 
     # the `request.referer != request.url` condition prevents logging the view after
     # placing a hold redirects back to the item page
-    if request.get? && (request.referer != request.url)
+    if request.referer != request.url
       ahoy.track "Item viewed", item_id: @item.id, referrer_url: request.referer
     end
   end
