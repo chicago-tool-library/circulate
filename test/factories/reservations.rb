c94929772bd3ad971f2ce55edca6db09dfc2a88b
Michael Crismali <crismali@users.noreply.github.com>
2024-07-26 22:18:34 -0500

    Added a review notes tab to the reservation show page (#1588)
    # What it does
    
    After a reservation is reviewed (approved or rejected) a tab called
    "Review Notes" appears that lets admins see the review notes.
    
    # Why it is important
    
    Takes care of #1533 
    
    # UI Change Screenshot
    
    ![Screenshot 2024-07-16 at 1 00
    50 AM](https://github.com/user-attachments/assets/a6eb9480-ebd8-4044-b929-d2ebb1682fae)
    
    # Implementation notes
    
    My text editor auto-formatted the scss. I can undo that so it's only the
    class I added, but since the code and the HTML have their own linters
    the changes seem reasonable to me.
    
    I continued the pattern for the other tabs on the reservation show page.
    Feels a little heavy to make a full request just for the review notes,
    but I think keeping the tab implementations consistent is probably more
    important.

--- test/factories/reservations.rb
+++ test/factories/reservations.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     sequence(:name) { |n| "A reservation ##{n}" }
     started_at { Time.current.at_beginning_of_day }
     ended_at { 1.week.since.at_beginning_of_day }
+
+    trait :approved do
+      status { "approved" }
+      notes { "Excited to approve #{name}!" }
+    end
   end
 end
