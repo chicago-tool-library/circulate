ef7861a6fe445218d4f1dec2d681a2a67e55d1ef
Jim Benton <jim@autonomousmachine.com>
2024-10-16 16:18:33 -0500

    Try to use magick command and fallback to convert.

--- app/lib/certificate/generator.rb
+++ app/lib/certificate/generator.rb
@@ -5,9 +5,16 @@ module Certificate
     end
 
     def generate
+      # see if magick command exists, if not, fall back to convert
+      command = command_exists?("magick") ? "magick" : "convert"
       @tempfile = Tempfile.new
-      `convert #{File.dirname(__FILE__)}/template.jpg -fill black -pointsize 58 -draw "text 1190,970 '#{@code}'" #{@tempfile.path}`
+      `#{command} #{File.dirname(__FILE__)}/template.jpg -fill black -pointsize 58 -draw "text 1190,970 '#{@code}'" #{@tempfile.path}`
       @tempfile.path
+    end
+
+    def command_exists?(name)
+      `which #{name}`
+      $?.success?
     end
   end
 end
