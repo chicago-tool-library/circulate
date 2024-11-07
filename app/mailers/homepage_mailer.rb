1e94de2fc320903a5f76d1c6195a0bd8328a70ed
Michael Crismali <michael@crismali.com>
2024-11-07 17:26:01 -0600

    remove homepage stuff

--- app/mailers/homepage_mailer.rb
+++
@@ -1,7 +0,0 @@
-class HomepageMailer < ApplicationMailer
-  def inquiry
-    @homepage_inquiry = params[:homepage_params]
-
-    mail(to: "team@circulate.software", subject: "[Circulate Inquiry]")
-  end
-end
