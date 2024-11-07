db4a3d2c8d701a6ed72ccee5fc4d0de82072cf00
Michael Crismali <michael@crismali.com>
2024-11-07 17:51:13 -0600

    Removed dead js controller, dead styles; removed admin only styles from application styles

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,64 +2,61 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from './application'
+import { application } from "./application";
 
-import AlertController from './alert_controller'
+import AlertController from "./alert_controller";
 
-import AppointmentDateController from './appointment_date_controller'
+import AppointmentDateController from "./appointment_date_controller";
 
-import AutocompleteController from './autocomplete_controller'
+import AutocompleteController from "./autocomplete_controller";
 
-import CollapseController from './collapse_controller'
+import CollapseController from "./collapse_controller";
 
-import ConditionalFieldController from './conditional_field_controller'
+import ConditionalFieldController from "./conditional_field_controller";
 
-import DynamicFieldsController from './dynamic_fields_controller'
+import DynamicFieldsController from "./dynamic_fields_controller";
 
-import EmailSettingsEditorController from './email_settings_editor_controller'
+import EmailSettingsEditorController from "./email_settings_editor_controller";
 
-import FindToolController from './find_tool_controller'
+import FindToolController from "./find_tool_controller";
 
-import HoldOrderController from './hold_order_controller'
+import HoldOrderController from "./hold_order_controller";
 
-import ImageEditorController from './image_editor_controller'
+import ImageEditorController from "./image_editor_controller";
 
-import ItemFilterController from './item_filter_controller'
+import ItemFilterController from "./item_filter_controller";
 
-import ModalController from './modal_controller'
+import ModalController from "./modal_controller";
 
-import MultiSelectController from './multi_select_controller'
+import MultiSelectController from "./multi_select_controller";
 
-import NotesController from './notes_controller'
+import NotesController from "./notes_controller";
 
-import RequestItemController from './request_item_controller'
+import ReservationDatesController from "./reservation_dates_controller";
 
-import ReservationDatesController from './reservation_dates_controller'
+import SidebarController from "./sidebar_controller";
 
-import SidebarController from './sidebar_controller'
+import TagEditorController from "./tag_editor_controller";
 
-import TagEditorController from './tag_editor_controller'
+import ToggleController from "./toggle_controller";
 
-import ToggleController from './toggle_controller'
-
-import TreeNavController from './tree_nav_controller'
-application.register('alert', AlertController)
-application.register('appointment-date', AppointmentDateController)
-application.register('autocomplete', AutocompleteController)
-application.register('collapse', CollapseController)
-application.register('conditional-field', ConditionalFieldController)
-application.register('dynamic-fields', DynamicFieldsController)
-application.register('email-settings-editor', EmailSettingsEditorController)
-application.register('find-tool', FindToolController)
-application.register('hold-order', HoldOrderController)
-application.register('image-editor', ImageEditorController)
-application.register('item-filter', ItemFilterController)
-application.register('modal', ModalController)
-application.register('multi-select', MultiSelectController)
-application.register('notes', NotesController)
-application.register('request-item', RequestItemController)
-application.register('reservation-dates', ReservationDatesController)
-application.register('sidebar', SidebarController)
-application.register('tag-editor', TagEditorController)
-application.register('toggle', ToggleController)
-application.register('tree-nav', TreeNavController)
+import TreeNavController from "./tree_nav_controller";
+application.register("alert", AlertController);
+application.register("appointment-date", AppointmentDateController);
+application.register("autocomplete", AutocompleteController);
+application.register("collapse", CollapseController);
+application.register("conditional-field", ConditionalFieldController);
+application.register("dynamic-fields", DynamicFieldsController);
+application.register("email-settings-editor", EmailSettingsEditorController);
+application.register("find-tool", FindToolController);
+application.register("hold-order", HoldOrderController);
+application.register("image-editor", ImageEditorController);
+application.register("item-filter", ItemFilterController);
+application.register("modal", ModalController);
+application.register("multi-select", MultiSelectController);
+application.register("notes", NotesController);
+application.register("reservation-dates", ReservationDatesController);
+application.register("sidebar", SidebarController);
+application.register("tag-editor", TagEditorController);
+application.register("toggle", ToggleController);
+application.register("tree-nav", TreeNavController);
