178705b8158b83561a30e1aca1280bda1d09bcdf
Michael Crismali <michael@crismali.com>
2024-11-25 15:20:33 -0600

    added sorting

--- app/controllers/admin/reports/items_in_maintenance_controller.rb
+++ app/controllers/admin/reports/items_in_maintenance_controller.rb
@@ -2,7 +2,43 @@ module Admin
   module Reports
     class ItemsInMaintenanceController < BaseController
       def index
-        @tickets = Ticket.all.includes(item: [:borrow_policy, :categories]).where.not(status: %w[resolved retired])
+        @tickets = ticket_query
+          .joins(:item)
+          .merge(item_query)
+          .includes(item: [:borrow_policy, :categories])
+      end
+
+      private
+
+      def ticket_query
+        type, column = current_sort
+        query = Ticket.all.where.not(status: %w[resolved retired])
+
+        if type == "ticket" && ["title", "status", "created_at", "updated_at"].include?(column)
+          query = query.order(column => current_direction)
+        end
+
+        query
+      end
+
+      def item_query
+        type, column = current_sort
+        query = Item.all
+
+        if type == "item" && ["number", "status", "name"].include?(column)
+          query = query.order(column => current_direction)
+        end
+
+        query
+      end
+
+      def current_sort
+        sort = params[:sort] || "ticket-created_at"
+        sort.split("-")
+      end
+
+      def current_direction
+        params[:direction] || "desc"
       end
     end
   end
