6ba1477c08e5a4c2aff9fe232fd429d13ec0f223
Michael Crismali <michael@crismali.com>
2024-11-26 12:11:19 -0600

    Lint fixes

--- app/controllers/admin/reports/items_in_maintenance_controller.rb
+++ app/controllers/admin/reports/items_in_maintenance_controller.rb
@@ -2,7 +2,7 @@ module Admin
   module Reports
     class ItemsInMaintenanceController < BaseController
       def index
-        params[:q] ||= {"s"=>"created_at desc"}
+        params[:q] ||= {"s" => "created_at desc"}
         @q = Ticket.all.where.not(status: %w[resolved retired]).ransack(params[:q])
         @tickets = @q.result.includes(:item)
       end
