b0e860580316dd429fc5b676ef10f446dd524802
Jim Benton <jim@autonomousmachine.com>
2025-02-06 20:01:14 -0600

    Use a Hash as the default value.

--- db/migrate/20250202194446_add_myturn_meta_to_reservable_items.rb
+++ db/migrate/20250202194446_add_myturn_meta_to_reservable_items.rb
@@ -1,5 +1,5 @@
 class AddMyturnMetaToReservableItems < ActiveRecord::Migration[7.2]
   def change
-    add_column :reservable_items, :myturn_metadata, :jsonb, default: "{}"
+    add_column :reservable_items, :myturn_metadata, :jsonb, default: {}
   end
 end
