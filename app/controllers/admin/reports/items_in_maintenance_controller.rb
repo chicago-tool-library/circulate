0db45b96c94a43455f3856bc7f22c3a3d40d3d2d
Renee Wah <rwah@illinois.edu>
2025-02-18 14:45:39 -0600

    basic tag filter form features

--- app/controllers/admin/reports/items_in_maintenance_controller.rb
+++ app/controllers/admin/reports/items_in_maintenance_controller.rb
@@ -3,9 +3,10 @@ module Admin
     class ItemsInMaintenanceController < BaseController
       def index
         params[:q] ||= {"s" => "created_at desc"}
-        @q = Ticket.all.where.not(status: %w[resolved retired]).ransack(params[:q])
+        @q = Ticket.all.where.not(status: %w[resolved retired]).has_tags(params[:tags_name_in]).ransack(params[:q])
         @tickets = @q.result.includes(:item)
       end
+
     end
   end
 end
