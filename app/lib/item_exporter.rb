6642ee0eb75dc479fc6ba104354bd142f7fba25f
Jim Benton <jim@autonomousmachine.com>
2025-02-19 19:24:01 -0600

    Adjust fields in item and member exporters.

--- app/lib/item_exporter.rb
+++ app/lib/item_exporter.rb
@@ -6,18 +6,22 @@ class ItemExporter
   end
 
   def export(stream)
+    converter = HTMLToMarkdown.new
     columns = %w[
-      id name description size brand model serial strength power_source
+      size brand model serial strength power_source
       other_names quantity checkout_notice status
       location_area location_shelf
       purchase_link purchase_price myturn_item_type url
       created_at updated_at
     ]
     headers = [
+      "id",
       "complete_number",
       "code",
       "number",
+      "name",
       "categories",
+      "description",
       "item_url",
       *columns
     ]
@@ -25,10 +29,13 @@ class ItemExporter
     @items.includes(:borrow_policy, :category_nodes, :rich_text_description).in_batches(of: 100) do |items|
       items.each do |item|
         stream.write(CSV.generate_line([
+          item.id,
           item.complete_number,
           item.borrow_policy.code,
           item.number,
+          item.name,
           item.category_nodes.map { |cn| cn.path_names.join("//") }.sort.join("; "),
+          converter.convert(item.description),
           "https://app.chicagotoollibrary.org/admin/items/#{item.id}",
           *columns.map { |c| item.send(c) }
         ]))
