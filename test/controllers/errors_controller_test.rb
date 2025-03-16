7d4d95734701bd6332c1d4c24fed4770dd74adb2
Michael Crismali <crismali@users.noreply.github.com>
2025-03-16 11:08:19 -0500

    Upgrade to Rails 8 (#1879)
    # What it does
    
    Upgrades Rails' Ruby and JavaScript dependencies to 8.0!
    
    # Why it is important
    
    #1814 and just general keeping up with the times.
    
    # Implementation notes
    
    There's a constant reassignment warning when running the tests that
    happens when `load_tasks` is called in `test/tasks/holds_test.rb`. I
    tried a few things but haven't cracked it yet. It's pretty minor so I
    figure it shouldn't hold this up.
    
    Aside from that, check out the specific comments I've left.

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
-  %w[404 422 500 503].each do |status|
+  %w[404 406 422 500 503].each do |status|
     test "should get #{status}" do
       get "/#{status}"
       assert_response status.to_i
