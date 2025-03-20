c0051dc3522c65b9f46459853500d4349a4f3939
Michael Crismali <michael@crismali.com>
2025-03-20 16:13:49 -0500

    Added pagination

--- app/controllers/admin/borrow_policy_approvals_controller.rb
+++ app/controllers/admin/borrow_policy_approvals_controller.rb
@@ -1,10 +1,11 @@
 module Admin
   class BorrowPolicyApprovalsController < BaseController
+    include Pagy::Backend
     before_action :set_borrow_policy, only: [:index]
 
     def index
       @q = @borrow_policy.borrow_policy_approvals.order(created_at: :desc).ransack(params[:q])
-      @borrow_policy_approvals = @q.result.includes(:member)
+      @pagy, @borrow_policy_approvals = pagy(@q.result.includes(:member))
     end
 
     private
