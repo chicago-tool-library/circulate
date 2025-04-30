2d31f54491c727837298701362644cc059753b69
Michael Crismali <michael@crismali.com>
2025-04-30 15:00:50 -0500

    Drop the description column from the items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_10_163403) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_30_195751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -491,7 +491,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_10_163403) do
 
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
-    t.string "description"
     t.string "size"
     t.string "brand"
     t.string "model"
