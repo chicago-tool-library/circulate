3f76d353beb2abe933527acb26a628584d0f997c
Paul Hinze <phinze@phinze.com>
2025-05-02 15:16:57 -0500

    Dust off the Nix development setup on mac (#1923)
    # What it does
    
    Makes the Nix development setup work on Darwin machines again
    
    # Why it is important
    
    Not very important! Just allows me to hop between Linux and Mac
    machines. Also we had one other person try out the Nix setup and note
    that it was failing on Chromium issues
    
    # Implementation Notes
    
    The only non-nix-specific code change here is to eager load vips for the
    image_processing gem. I think this should be safe, and I'll make sure
    it's working ok on staging before rolling it out.

--- flake.nix
+++ flake.nix
@@ -26,59 +26,71 @@
       };
     in {
       devShell = with pkgs;
-        mkShell {
-          buildInputs = [
-            ruby
-            foreman
-            nodejs_20
+        mkShell ({
+            buildInputs = [
+              ruby
+              foreman
+              nodejs_20
 
-            # Make sure yarn uses the version-specific node package specified
-            # above
-            (yarn.override {nodejs = nodejs_20;})
+              # Make sure yarn uses the version-specific node package specified
+              # above
+              (yarn.override {nodejs = nodejs_20;})
 
-            # needed to build pg gem, even though we'll run the db from Docker
-            postgresql_15
+              # needed to build pg gem, even though we'll run the db from Docker
+              postgresql_15
 
-            # lib/certificate/generator.rb shells out to convert
-            # ghostscript override fixes issue with with convert being unable
-            # to find a font, workaround copied from
-            # https://github.com/NixOS/nixpkgs/pull/246444
-            (imagemagick.override {ghostscriptSupport = true;})
+              # lib/certificate/generator.rb shells out to convert
+              # ghostscript override fixes issue with with convert being unable
+              # to find a font, workaround copied from
+              # https://github.com/NixOS/nixpkgs/pull/246444
+              (imagemagick.override {ghostscriptSupport = true;})
 
-            # for system tests
-            chromium
+              # install chromium for system tests on non-mac platforms
+              (lib.optional (!stdenv.isDarwin) chromium)
 
-            # for linters and git hooks
-            lefthook
+              # for linters and git hooks
+              lefthook
 
-            # for deployment
-            heroku
+              # for deployment
+              heroku
 
-            # needed to build psych gem, which is in the dependency tree as of rails 7.1
-            libyaml
+              # needed to build psych gem, which is in the dependency tree as of rails 7.1
+              libyaml
 
-            # needed for vips ffi to work
-            glib
-            vips
-          ];
+              # needed for vips ffi to work
+              glib
+              vips
+            ];
 
-          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [glib vips]}";
+            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [glib vips]}";
 
-          # Keep gems installed in a subdirectory
-          BUNDLE_PATH = "./vendor/bundle";
+            # Keep gems installed in a subdirectory
+            BUNDLE_PATH = "./vendor/bundle";
 
-          # Point to Docker-hosted db server, using the same settings as
-          # config/docker.env
-          DATABASE_URL = "postgres://localhost:5435";
-          PGUSER = "postgres";
+            # Point to Docker-hosted db server, using the same settings as
+            # config/docker.env
+            DATABASE_URL = "postgres://localhost:5435";
+            PGUSER = "postgres";
 
-          # We'll run system tests in headless mode
-          HEADLESS = "true";
-          PARALLEL_WORKERS = "3";
-          PLAYWRIGHT_CHROME_BINARY = "${pkgs.chromium}/bin/chromium";
+            # We'll run system tests in headless mode
+            HEADLESS = "true";
+            PARALLEL_WORKERS = "3";
 
-          # Set a general env variable that other setup code can use.
-          USING_NIX = "1";
-        };
+            NIX_IS_DARWIN =
+              if stdenv.isDarwin
+              then "true"
+              else "false";
+          }
+          // (lib.optionalAttrs (stdenv.isDarwin) {
+            # Work around occasional segfault on MacOS, see https://github.com/rails/rails/issues/38560#issuecomment-1881733872
+            PGGSSENCMODE = "disable";
+
+            # Fix Fontconfig warning: Cannot load default config file: No such file: (null)
+            FONTCONFIG_FILE = "${fontconfig.out}/etc/fonts/fonts.conf";
+          })
+          // (lib.optionalAttrs (!stdenv.isDarwin) {
+            # On non-mac hosts, use installed chromium binary
+            PLAYWRIGHT_CHROME_BINARY = "${pkgs.chromium}/bin/chromium";
+          }));
     });
 }
