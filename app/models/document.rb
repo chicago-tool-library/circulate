40d3f1f0b9bffd74734549088093f9eb9994d321
Jim Benton <jim@autonomousmachine.com>
2025-06-14 09:39:28 -0500

    Fix lint error

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,7 +6,7 @@ class Document < ApplicationRecord
   enum :code, {
     agreement: "agreement",
     code_of_conduct: "code_of_conduct",
-    borrow_policy: "borrow_policy",
+    borrow_policy: "borrow_policy"
   }, validate: true
 
   acts_as_tenant :library
