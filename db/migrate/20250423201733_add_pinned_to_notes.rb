8a451448983a94722e1962787377742f3e8d973a
fgonza23 <42500480+fgonza23@users.noreply.github.com>
2025-07-08 21:29:19 -0500

    Update db/migrate/20250423201733_add_pinned_to_notes.rb
    Co-authored-by: Michael Crismali <crismali@users.noreply.github.com>

--- db/migrate/20250423201733_add_pinned_to_notes.rb
+++ db/migrate/20250423201733_add_pinned_to_notes.rb
@@ -1,5 +1,5 @@
 class AddPinnedToNotes < ActiveRecord::Migration[8.0]
   def change
-    add_column :notes, :pinned, :boolean
+    add_column :notes, :pinned, :boolean, default: false, null: false
   end
 end
