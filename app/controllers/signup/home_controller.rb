eaf3fdc72e9c9a56a011828bea92d9e3baed9477
Jim Benton <3331+jim@users.noreply.github.com>
2025-11-19 08:36:54 -0600

    Replace organization signup links. Fixes #2043. (#2045)
    # What it does
    
    Replaces the organization signup links with those to the camping
    inventory.
    
    # UI Change Screenshot
    
    <img width="307" height="150" alt="Screenshot 2025-11-18 at 7 38 11 PM"
    src="https://github.com/user-attachments/assets/f9ab6606-b8d4-4479-9d8c-d8a4ffbeb76c"
    />
    <img width="716" height="589" alt="Screenshot 2025-11-18 at 7 38 26 PM"
    src="https://github.com/user-attachments/assets/22e24d0f-7a36-49ee-8f54-2b21e47d7655"
    />

--- app/controllers/signup/home_controller.rb
+++ app/controllers/signup/home_controller.rb
@@ -4,11 +4,6 @@ module Signup
 
     def index
       @hide_steps = true
-      @org_signup_url = if FeatureFlags.group_lending_enabled?
-        signup_organizations_policies_url
-      else
-        ENV.fetch("ORGANIZATION_SIGNUP_URL")
-      end
     end
   end
 end
