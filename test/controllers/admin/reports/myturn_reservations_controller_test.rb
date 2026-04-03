1012272d555f13e4a6055a7689c28b5674d7c12b
Paul Hinze <phinze@phinze.com>
2026-04-03 16:20:50 -0500

    Fix flaky test in CI by using ENV vars instead of stub
    The parallel test runner forks processes, which can interfere with
    Minitest stubs on module methods. Setting ENV vars directly is more
    reliable. Also hardcode the MyTurn date format in test data to
    avoid timezone-dependent iso8601 differences between local and CI.

--- test/controllers/admin/reports/myturn_reservations_controller_test.rb
+++ test/controllers/admin/reports/myturn_reservations_controller_test.rb
@@ -8,22 +8,30 @@ module Admin
       setup do
         @user = create(:admin_user)
         sign_in @user
+        ENV["MYTURN_USERNAME"] = "test"
+        ENV["MYTURN_PASSWORD"] = "test"
+      end
+
+      teardown do
+        ENV.delete("MYTURN_USERNAME")
+        ENV.delete("MYTURN_PASSWORD")
       end
 
       test "renders index when myturn is not configured" do
-        Myturn.stub(:configured?, false) do
-          get admin_reports_myturn_reservations_url
-          assert_response :success
-          assert_select "p", /not configured/i
-        end
+        ENV.delete("MYTURN_USERNAME")
+        ENV.delete("MYTURN_PASSWORD")
+
+        get admin_reports_myturn_reservations_url
+        assert_response :success
+        assert_select "p", /not configured/i
       end
 
       test "renders index with reservations from myturn" do
-        today = Date.current
+        today = Time.current.to_date
         reservations = [
           {
             "id" => 1,
-            "dateTime" => today.to_time.iso8601,
+            "dateTime" => "#{today}T00:00:00.000-05:00",
             "pickupStartTime" => "10:00 AM",
             "pickupEndTime" => "11:00 AM",
             "length" => 7,
@@ -34,7 +42,7 @@ module Admin
           },
           {
             "id" => 2,
-            "dateTime" => (today - 5.days).to_time.iso8601,
+            "dateTime" => "#{today - 5.days}T00:00:00.000-05:00",
             "pickupStartTime" => "1:00 PM",
             "pickupEndTime" => "2:00 PM",
             "length" => 5,
@@ -47,41 +55,35 @@ module Admin
 
         stub_client = StubMyturnClient.new(reservations)
 
-        Myturn.stub(:configured?, true) do
-          Myturn::Client.stub(:new, stub_client) do
-            get admin_reports_myturn_reservations_url
-            assert_response :success
+        Myturn::Client.stub(:new, stub_client) do
+          get admin_reports_myturn_reservations_url
+          assert_response :success
 
-            # Jane has a pickup today
-            assert_select "td", /Jane/
+          # Jane has a pickup today
+          assert_select "td", /Jane/
 
-            # John's reservation started 5 days ago with length 5, so dropoff is today
-            assert_select "td", /John/
-          end
+          # John's reservation started 5 days ago with length 5, so dropoff is today
+          assert_select "td", /John/
         end
       end
 
       test "renders index with day parameter" do
         stub_client = StubMyturnClient.new([])
 
-        Myturn.stub(:configured?, true) do
-          Myturn::Client.stub(:new, stub_client) do
-            get admin_reports_myturn_reservations_url(day: "2026-06-15")
-            assert_response :success
-            assert_select "b", "Monday, June 15, 2026"
-          end
+        Myturn::Client.stub(:new, stub_client) do
+          get admin_reports_myturn_reservations_url(day: "2026-06-15")
+          assert_response :success
+          assert_select "b", "Monday, June 15, 2026"
         end
       end
 
       test "handles api errors gracefully" do
         stub_client = StubMyturnClient.new(RuntimeError.new("Connection refused"))
 
-        Myturn.stub(:configured?, true) do
-          Myturn::Client.stub(:new, stub_client) do
-            get admin_reports_myturn_reservations_url
-            assert_response :success
-            assert_select ".toast-error"
-          end
+        Myturn::Client.stub(:new, stub_client) do
+          get admin_reports_myturn_reservations_url
+          assert_response :success
+          assert_select ".toast-error"
         end
       end
 
